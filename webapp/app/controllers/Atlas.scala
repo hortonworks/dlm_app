723b56290a8e809780259e5e4f9f616d2f5d7aea
Ashwin <ashwin@vidyash.com>
2016-12-13 06:51:17 +0100

    DP-15 Making AtlasApiCache Threadsafe

--- webapp/app/controllers/Atlas.scala
+++ webapp/app/controllers/Atlas.scala
@@ -1,9 +1,11 @@
 package controllers
 
+import akka.actor.ActorRef
 import com.google.inject.Inject
+import com.google.inject.name.Named
 import com.hw.dp.service.cluster.DataConstraints
 import com.hw.dp.services.atlas.AtlasHiveApi
-import internal.AtlasApiCache
+import internal.{AtlasApiCache, GetApi}
 import internal.auth.Authenticated
 import internal.persistence.DataStorage
 import models.JsonResponses
@@ -11,13 +13,15 @@ import play.api.libs.json.Json
 import play.api.mvc._
 
 import scala.concurrent.Future
+import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
-
+import akka.pattern.ask
+import akka.util.Timeout
 
 /**
   * Get settings to show in various parts of the APP
   */
-class Atlas @Inject() (val atlasApiCache:AtlasApiCache,storage: DataStorage) extends Controller {
+class Atlas @Inject() ( @Named("atlasApiCache") val atlasApiCache:ActorRef,storage: DataStorage) extends Controller {
 
 
   import com.hw.dp.services.atlas.Hive._
@@ -29,11 +33,14 @@ class Atlas @Inject() (val atlasApiCache:AtlasApiCache,storage: DataStorage) ext
     }
   }
 
+
+
   private def getApi(clusterHost: String, datacenter: String): Future[AtlasHiveApi] = {
+    implicit val timeout = Timeout(120 seconds)
     for {
       ambari <- storage.loadCluster(clusterHost, datacenter)
       cluster <- storage.loadClusterInformation(clusterHost, datacenter)
-      api <- atlasApiCache.getApi(ambari.get, cluster.get)
+      api <- (atlasApiCache ? GetApi(ambari.get, cluster.get)).mapTo[Future[AtlasHiveApi]].flatMap(f => f )
     } yield {
       api
     }
