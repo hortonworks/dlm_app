5eca27a7c8b04d8de6133e5a4e2f9321ce310b9b
Ashwin <ashwin@vidyash.com>
2016-12-30 00:06:06 +0100

    Fix bug on saving empty filters

--- webapp/app/assets/app/models/data-filter-wrapper.ts
+++ webapp/app/assets/app/models/data-filter-wrapper.ts
@@ -1,14 +1,19 @@
 import {DataFilter} from './data-filter';
 export class DataFilterWrapper {
 
-    constructor(public dataFilter: DataFilter, public data:any [] = []) {}
+    constructor(public dataFilter: DataFilter, public data: any [] = []) {
+    }
 
     public static createDataFilters(dataFilter: DataFilter): DataFilterWrapper {
         return new DataFilterWrapper(<DataFilter>JSON.parse(JSON.stringify(dataFilter)), []);
     }
 
     public static getDataFilters(dataFilterWrappers: DataFilterWrapper[]): DataFilter[] {
-        let filters = dataFilterWrappers.map(wrapper => wrapper.dataFilter[0]);
+        let filters: DataFilter[] = [];
+        dataFilterWrappers.forEach(wrapper => {
+            if (wrapper.dataFilter[0] != null)
+                filters.push(wrapper.dataFilter[0]);
+        });
         return filters.length === 0 ? [] : filters;
     }
 }
