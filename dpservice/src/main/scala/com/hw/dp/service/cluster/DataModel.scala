2e58b4efb7069e488c85955d0e5c163f96c750a5
Ashwin <ashwin@vidyash.com>
2016-12-31 21:48:50 +0100

    Added  lastModified

--- dpservice/src/main/scala/com/hw/dp/service/cluster/DataModel.scala
+++ dpservice/src/main/scala/com/hw/dp/service/cluster/DataModel.scala
@@ -1,5 +1,7 @@
 package com.hw.dp.service.cluster
 
+import java.util.Date
+
 object DataModel {
 
   case class DataSet(ambariHost: String,
@@ -12,7 +14,8 @@ object DataModel {
                      hBaseFilters :Seq[DataFilter],
                      fileFilters:Seq[DataFilter],
                      properties: Map[String, String],
-                     userName:Option[String] = None)
+                     userName:Option[String] = None,
+                     lastModified:Date = new Date())
   // for Hive
   //path - field/column/column_attribute
   case class DataFilter(predicate: String, qualifier: String = "field")
