3a20a5bf8f1e7cd8c011e6a483b74b9506c70bfa
Abhishek Kumar <labs.abhi@github.com>
2017-01-02 15:50:31 +0530

    Listening to changes on props change for ranger

--- webapp/app/assets/app/components/ranger-policies/ranger-policies.component.ts
+++ webapp/app/assets/app/components/ranger-policies/ranger-policies.component.ts
@@ -1,13 +1,27 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, OnInit, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {RangerPoliciesService} from '../../services/ranger-policies.service';
 import {RangerPolicies} from '../../models/ranger-policies';
+import Rx from 'rxjs/Rx';
 
 @Component({
     selector: 'ranger-policies',
     templateUrl: 'assets/app/components/ranger-policies/ranger-policies.component.html',
     styleUrls: ['assets/app/components/ranger-policies/ranger-policies.component.css']
 })
-export class RangerPoliciesComponent implements OnInit {
+export class RangerPoliciesComponent implements OnInit, OnChanges {
+
+  rxInputChange: Rx.Subject<{
+    resourceId: string,
+    resourceType: string,
+    dataLakeId: string,
+    clusterId: string
+  }> = new Rx.Subject<{
+    resourceId: string,
+    resourceType: string,
+    dataLakeId: string,
+    clusterId: string
+  }>();
+
     @Input()
     resourceId: string = '';
     @Input()
@@ -21,11 +35,24 @@ export class RangerPoliciesComponent implements OnInit {
 
     constructor(private rangerPoliciesService: RangerPoliciesService) {
 
+      this.rxInputChange
+        .flatMap(({resourceId, resourceType, dataLakeId, clusterId}) => this.rangerPoliciesService.get(resourceId, resourceType, dataLakeId, clusterId))
+        .subscribe(policies => this.policesData = RangerPolicies.getData(policies));
+
     }
 
     ngOnInit() {
-        this.rangerPoliciesService.get(this.resourceId,this.resourceType,this.dataLakeId,this.clusterId).subscribe(policiesData => {
-            this.policesData = RangerPolicies.getData(policiesData);
+      //
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+      if(this.resourceId && this.resourceType && this.dataLakeId && this.clusterId) {
+        this.rxInputChange.next({
+          resourceId: this.resourceId,
+          resourceType: this.resourceType,
+          dataLakeId: this.dataLakeId,
+          clusterId: this.clusterId
         });
+      }
     }
 }
