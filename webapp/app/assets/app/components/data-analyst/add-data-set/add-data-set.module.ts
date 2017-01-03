36874114b3ba0737f9b7dd3c4f5c00d3657158a4
iraghumitra <raghumitra.ksv@gmail.com>
2017-01-03 19:15:56 +0530

    Corrected the URL from /ui/data-lake/add to /ui/add-data-lake as this is conflicting with view data-lake

--- webapp/app/assets/app/components/data-analyst/add-data-set/add-data-set.module.ts
+++ webapp/app/assets/app/components/data-analyst/add-data-set/add-data-set.module.ts
@@ -7,9 +7,10 @@ import {DataPlaneSearchModule} from '../../../shared/data-plane-search/data-plan
 import {DropDownModule} from '../../../shared/dropdown/dropdown.module';
 import {SearchQueryService} from '../../../services/search-query.service';
 import {BreadcrumbModule} from '../../../shared/breadcrumb/breadcrumb.module';
+import {ChipsBarModule} from '../../../shared/chips-bar/chips-bar.module';
 
 @NgModule({
-    imports: [routing, SharedModule, DataPlaneSearchModule, DropDownModule, BreadcrumbModule],
+    imports: [routing, SharedModule, DataPlaneSearchModule, DropDownModule, BreadcrumbModule, ChipsBarModule],
     exports: [AddDataSetComponent],
     declarations: [AddDataSetComponent],
     providers: [DataSetService, SearchQueryService],
