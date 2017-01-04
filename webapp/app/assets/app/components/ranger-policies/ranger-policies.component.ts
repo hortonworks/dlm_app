615c0c381087af23d4c8a1859ef4e39cc79a40c1
Abhishek Kumar <labs.abhi@github.com>
2017-01-04 13:49:49 +0530

    Ranger graphs

--- webapp/app/assets/app/components/ranger-policies/ranger-policies.component.ts
+++ webapp/app/assets/app/components/ranger-policies/ranger-policies.component.ts
@@ -1,14 +1,16 @@
-import {Component, OnInit, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {Component, OnInit, Input, OnChanges, SimpleChanges, AfterViewInit} from '@angular/core';
 import {RangerService} from '../../services/ranger.service';
 import {RangerPolicies} from '../../models/ranger-policies';
 import Rx from 'rxjs/Rx';
+
+declare let dX:any;
 
 @Component({
     selector: 'ranger-policies',
     templateUrl: 'assets/app/components/ranger-policies/ranger-policies.component.html',
     styleUrls: ['assets/app/components/ranger-policies/ranger-policies.component.css']
 })
-export class RangerPoliciesComponent implements OnInit, OnChanges {
+export class RangerPoliciesComponent implements OnInit, OnChanges, AfterViewInit {
 
   rxInputChange: Rx.Subject<{
     resourceId: string,
@@ -32,12 +34,28 @@ export class RangerPoliciesComponent implements OnInit, OnChanges {
     clusterId: string = '';
 
     policies: RangerPolicies[] = [];
+    access: any[] = [];
+    users: any[] = [];
 
     constructor(private rangerPoliciesService: RangerService) {
 
       this.rxInputChange
         .flatMap(({resourceId, resourceType, dataLakeId, clusterId}) => this.rangerPoliciesService.getPolicies(resourceId, resourceType, dataLakeId, clusterId))
         .subscribe(policies => this.policies = RangerPolicies.getData(policies));
+
+      this.rxInputChange
+        .flatMap(({resourceId, resourceType, dataLakeId, clusterId}) => this.rangerPoliciesService.getAccess(resourceId, resourceType, dataLakeId, clusterId))
+        .subscribe(access => {
+          this.access = access;
+          this.drawChart('#ranger_barchart_access', access);
+        });
+
+      this.rxInputChange
+        .flatMap(({resourceId, resourceType, dataLakeId, clusterId}) => this.rangerPoliciesService.getUsers(resourceId, resourceType, dataLakeId, clusterId))
+        .subscribe(users => {
+          this.users = users;
+          this.drawChart('#ranger_barchart_users', users);
+        });
 
     }
 
@@ -54,5 +72,66 @@ export class RangerPoliciesComponent implements OnInit, OnChanges {
           clusterId: this.clusterId
         });
       }
+    }
+
+    ngAfterViewInit() {
+      // if(this.resourceId && this.resourceType && this.dataLakeId && this.clusterId) {
+      //   this.rxInputChange.next({
+      //     resourceId: this.resourceId,
+      //     resourceType: this.resourceType,
+      //     dataLakeId: this.dataLakeId,
+      //     clusterId: this.clusterId
+      //   });
+      // }
+    }
+
+    drawChart(domId: string, data: any[]) {
+
+      const d3 = dX;
+      const svg = d3.select(domId),
+        margin = {top: 20, right: 20, bottom: 30, left: 40},
+        width = +svg.attr('width') - margin.left - margin.right,
+        height = +svg.attr('height') - margin.top - margin.bottom;
+
+      const x =
+        d3
+          .scaleBand()
+          .rangeRound([0, width])
+          .padding(0.1);
+      x.domain(data.map(function(d) { return d.key; }));
+
+      const y =
+        d3
+        .scaleLinear()
+        .range([height, 0]);
+      y.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+      const g = svg.append('g')
+          .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+      g.append('g')
+          .attr('class', 'axis axis--x')
+          .attr('transform', 'translate(0,' + height + ')')
+          .call(d3.axisBottom(x));
+
+      const maxValueOfY = d3.max(data, function(d) { return d.value; });
+      const maxTicks = maxValueOfY < 5 ? maxValueOfY : maxValueOfY / 5;
+      g.append('g')
+          .attr('class', 'axis axis--y')
+          .call(
+            d3
+              .axisLeft(y)
+              .tickFormat(d3.format('.0f'))
+              .ticks(maxTicks)
+          );
+
+        g.selectAll('.bar')
+          .data(data)
+          .enter().append('rect')
+            .attr('class', 'bar')
+            .attr('x', function(d) { return x(d.key); })
+            .attr('width', x.bandwidth())
+            .attr('y', function(d) { return y(d.value); })
+            .attr('height', function(d) { return height - y(d.value); });
     }
 }
--- webapp/app/assets/app/services/ranger.service.ts
+++ webapp/app/assets/app/services/ranger.service.ts
@@ -21,4 +21,46 @@ export class RangerService {
             .map(HttpUtil.extractData).catch(HttpUtil.handleError);
     }
 
+    public getAccess(
+      resourceId: string,
+      resourceType: string,
+      dataLakeId: string,
+      clusterId: string
+    ): Observable<any[]> {
+      return this.http
+        .get(`${this.url}/access?clusterHost=${clusterId}&dc=${dataLakeId}&rt=${resourceType}&r=${resourceId}`)
+        .map(HttpUtil.extractData)
+        .map(data => data.facet_counts && data.facet_counts.facet_fields && data.facet_counts.facet_fields.access)
+        .map(access => {
+          return access
+            .filter((cAccessItem, index) => index % 2 === 0 && cAccessItem !== null)
+            .map((cAccessKey, index) => ({
+              key: cAccessKey,
+              value: access[index * 2 + 1]
+            }));
+        })
+        .catch(HttpUtil.handleError);
+    }
+
+    public getUsers(
+      resourceId: string,
+      resourceType: string,
+      dataLakeId: string,
+      clusterId: string
+    ): Observable<any[]> {
+      return this.http
+        .get(`${this.url}/users?clusterHost=${clusterId}&dc=${dataLakeId}&rt=${resourceType}&r=${resourceId}`)
+        .map(HttpUtil.extractData)
+        .map(data => data.facet_counts && data.facet_counts.facet_fields && data.facet_counts.facet_fields.reqUser)
+        .map(reqUser => {
+          return reqUser
+            .filter((cUserItem, index) => index % 2 === 0 && cUserItem !== null)
+            .map((cUserKey, index) => ({
+              key: cUserKey,
+              value: reqUser[index * 2 + 1]
+            }));
+        })
+        .catch(HttpUtil.handleError);
+    }
+
 }
