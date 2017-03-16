44e2a6918cc8c4f27a3baf33021a2c0131c36e04
Abhishek Kumar <labs.abhi@github.com>
2017-03-16 12:25:20 +0530

    modifying routes

--- services/db-service/app/domain/API.scala
+++ services/db-service/app/domain/API.scala
@@ -3,10 +3,10 @@ package domain
 
 object API {
 
-  val users = "/api/v1/users"
-  val roles = "/api/v1/roles"
-  val locations = "/api/v1/locations"
-  val datalakes = "/api/v1/datalakes"
-  val clusters = "/api/v1/clusters"
+  val users = "/users"
+  val roles = "/roles"
+  val locations = "/locations"
+  val datalakes = "/datalakes"
+  val clusters = "/clusters"
 
 }
