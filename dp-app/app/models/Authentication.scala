a57b0dc1221a998d4f1bea820012df87cf121347
Abhishek Kumar <labs.abhi@github.com>
2017-03-20 12:07:52 +0530

    Removing unused code

--- dp-app/app/models/Authentication.scala
+++ dp-app/app/models/Authentication.scala
@@ -1,32 +1,10 @@
 package models
 
-import java.util.Date
-
-import play.api.libs.json._
-import play.api.libs.json.Reads._
-import play.api.libs.functional.syntax._
-
-case class UserRequest(username: String,
-                       password: String)
-
 case class Credential(id: String, password: String)
-
-
-
-
-//object User {
-//  val userTypes = Set("USER","ADMIN","SUPERUSER","ANALYSTADMIN","INFRAADMIN")
-//  val authTypes= Set("LDAP","LOCAL")
-//}
-//
-//
-//case class UserView(username: String, password: String, admin: Boolean,userType:String="USER")
 
 object JsonFormats {
 
   import play.api.libs.json.Json
-
-  implicit val userReqFormat = Json.format[UserRequest]
 
   implicit val credentialFormat = Json.format[Credential]
 
