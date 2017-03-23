72861e4f4bf31ec0117b9c5af02cebc465c68420
Abhishek Kumar <labs.abhi@github.com>
2017-03-23 21:25:24 +0530

    Filtered out dirty values

--- services/db-service/db/generators/migrate.js
+++ services/db-service/db/generators/migrate.js
@@ -3,11 +3,18 @@ var obj = JSON.parse(fs.readFileSync('./cities.json', 'utf8'));
 
 var stream = fs.createWriteStream("./locations.sql");
 stream.once('open', function(fd) {
-  obj
-  .filter(cLocation =>  /^[\x00-\xFF]*$/.test(cLocation.name) && cLocation.country.length > 0)
-  .forEach(cLocation => {
-    stream.write(`INSERT INTO dataplane.dp_locations (country, city, latitude, longitude) VALUES ('${cLocation.country}', '${cLocation.name}', ${cLocation.lat}, ${cLocation.lng});\n`);
-  });
+  const deduped =
+    obj
+    .filter(cLocation =>  /^[\x00-\xFF]*$/.test(cLocation.name) && cLocation.country.length > 0 && !isNaN(cLocation.lat) && !isNaN(cLocation.lng))
+    .reduce((accumulator, cLocation) => {
+      accumulator[`${cLocation.country}-${cLocation.name}`] = cLocation;
+      return accumulator;
+    }, {});
+  Object.keys(deduped)
+    .forEach(cLocationKey => {
+      const cLocation = deduped[cLocationKey];
+      stream.write(`INSERT INTO dataplane.dp_locations (country, city, latitude, longitude) VALUES ('${cLocation.country}', '${cLocation.name.replace(/'/g, "''")}', ${cLocation.lat}, ${cLocation.lng});\n`);
+    });
   stream.end();
 });
 // fails for cLocation = { country: 'AF', name: 'شرن', lat: '33.175678', lng: '68.730449' } and hence filtered out.
