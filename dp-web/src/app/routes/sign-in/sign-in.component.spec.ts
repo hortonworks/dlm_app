9a9c7f172b1176f42b96dfed9cc31f763de08d9e
geksilla <buzhordenis@gmail.com>
2017-03-25 00:54:06 +0200

    Fixed web app unit tests.
    * Added missed karma plugin packages
    * Fixed various unit tests issues

--- dp-web/src/app/routes/sign-in/sign-in.component.spec.ts
+++ dp-web/src/app/routes/sign-in/sign-in.component.spec.ts
@@ -1,14 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+import { AuthenticationService } from '../../services/authentication.service';
+import { BreadcrumbService } from '../../services/breadcrumb.service';
+import { Environment } from '../../environment';
 
 import { SignInComponent } from './sign-in.component';
 
 describe('SignInComponent', () => {
   let component: SignInComponent;
   let fixture: ComponentFixture<SignInComponent>;
+  let authenticationServiceStub: Object;
+  let environmentStub: Object;
+  let breadcrumbServiceStub: Object;
 
   beforeEach(async(() => {
+    authenticationServiceStub = {};
+    environmentStub = {};
+    breadcrumbServiceStub = {};
+
     TestBed.configureTestingModule({
-      declarations: [ SignInComponent ]
+      imports: [FormsModule, RouterTestingModule],
+      declarations: [ SignInComponent ],
+      providers: [
+        {provide: AuthenticationService, useValue: authenticationServiceStub},
+        {provide: Environment, useValue: environmentStub},
+        {provide: BreadcrumbService, useValue: breadcrumbServiceStub}
+      ]
     })
     .compileComponents();
   }));
--- dp-web/src/app/services/authentication.service.spec.ts
+++ dp-web/src/app/services/authentication.service.spec.ts
@@ -1,14 +1,33 @@
+import { HttpModule } from '@angular/http';
 import { TestBed, inject } from '@angular/core/testing';
 import { AuthenticationService } from './authentication.service';
 
 describe('AuthenticationService', () => {
+  let service;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpModule],
       providers: [AuthenticationService]
     });
   });
 
-  it('should ...', inject([AuthenticationService], (service: AuthenticationService) => {
-    expect(service).toBeTruthy();
+  beforeEach(inject([AuthenticationService], s => {
+    service = s;
   }));
+
+  describe('#isAuthenticated', () => {
+    it('should return true when user is authenticated', () => {
+      service.isUserAuthenticated = true;
+      expect(service.isAuthenticated()).toBeTruthy();
+    });
+  });
+
+  describe('#signOut', () => {
+    it('should sign out the user', () => {
+      service.isUserAuthenticated = true;
+      service.signOut();
+      expect(service.isAuthenticated()).toBeFalsy();
+    });
+  });
 });
