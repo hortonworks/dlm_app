61115ae0e0bf9f1aceaae4d94f2f6a4f4cbf2bcf
Abhishek Kumar <labs.abhi@github.com>
2017-03-27 20:17:10 +0530

    Completing merge

--- clients/db-client/src/main/scala/com/hortonworks/dataplane/db/LocationServiceImpl.scala
+++ clients/db-client/src/main/scala/com/hortonworks/dataplane/db/LocationServiceImpl.scala
@@ -5,7 +5,6 @@ import javax.inject.Singleton
 import com.hortonworks.dataplane.commons.domain.Entities._
 import com.hortonworks.dataplane.db.Webserice.LocationService
 import com.typesafe.config.Config
-import play.api.libs.json.{JsResult, Json}
 import play.api.libs.ws.{WSClient, WSResponse}
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -30,32 +29,8 @@ class LocationServiceImpl(config: Config)(implicit ws: WSClient)
   private def mapToLocations(res: WSResponse) = {
     res.status match {
       case 200 =>
-        extractEntity[Seq[Location]](res, r => (r.json \ "results").validate[Seq[Location]])
+        extractEntity[Seq[Location]](res, r => (r.json \ "results").validate[Seq[Location]].get)
       case _ => mapErrors(res)
     }
-  }
-
-  private def mapErrors(res: WSResponse) = {
-    Left(extractError(res, r => r.json.validate[Errors]))
-  }
-
-  private def extractEntity[T](
-      res: WSResponse,
-      f: WSResponse => JsResult[T]): Either[Errors, T] = {
-    println(res)
-    f(res)
-      .map({r =>
-        println(r)
-        Right(r)})
-      .getOrElse(Left(Errors(Seq(Error(
-        "500",
-        s"sCould not parse response from DB - ${Json.stringify(res.json)}")))))
-  }
-
-  private def extractError(res: WSResponse,
-                           f: WSResponse => JsResult[Errors]): Errors = {
-    if (res.body.isEmpty)
-      Errors()
-    f(res).map(r => r).getOrElse(Errors())
   }
 }
