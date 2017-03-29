16796a82715e2da24a158f672349b0bd4332b525
Abhishek Kumar <labs.abhi@github.com>
2017-03-29 22:24:50 +0530

    Fixing AOT with Angular 4.0

--- dp-web/src/app/components/dashboard/dashboard.routing.ts
+++
@@ -1,7 +0,0 @@
-import { ModuleWithProviders }  from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { DashboardComponent } from './dashboard';
-
-export const routing: ModuleWithProviders = RouterModule.forChild([
-    { path: '', component: DashboardComponent}
-]);
