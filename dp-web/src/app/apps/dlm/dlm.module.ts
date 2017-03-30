6ecf1d6d8855ee54b01fd137aaae78052a286985
Vivek Ratnavel Subramanian <vivekratnavel90@gmail.com>
2017-03-30 08:55:15 -0700

    BUG-78195 Implement basic routing for DLM (#43)
    * BUG-78195 Implement basic routing for DLM

--- dp-web/src/app/apps/dlm/dlm.module.ts
+++ dp-web/src/app/apps/dlm/dlm.module.ts
@@ -7,6 +7,12 @@ import { ClusterService } from './services/cluster.service';
 import { PolicyService } from './services/policy.service';
 import { JobService } from './services/job.service';
 import { MainComponent } from './pages/main/main.component';
+import { OverviewComponent } from './pages/overview/overview.component';
+import { ClustersComponent } from './pages/clusters/clusters.component';
+import { PairingsComponent } from './pages/pairings/pairings.component';
+import { PoliciesComponent } from './pages/policies/policies.component';
+import { JobsComponent } from './pages/jobs/jobs.component';
+import { HelpComponent } from './pages/help/help.component';
 
 @NgModule({
   imports: [
@@ -14,7 +20,15 @@ import { MainComponent } from './pages/main/main.component';
     routing,
     HttpModule
   ],
-  declarations: [MainComponent],
+  declarations: [
+    MainComponent,
+    OverviewComponent,
+    ClustersComponent,
+    PairingsComponent,
+    PoliciesComponent,
+    JobsComponent,
+    HelpComponent
+  ],
   providers: [
     ClusterService,
     JobService,
