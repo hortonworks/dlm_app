dc2b95e00dcaba71d3e07064bcda43b9c06eca94
Abhishek Kumar <labs.abhi@github.com>
2017-03-30 14:01:26 +0530

    Adding IdentityService

---
+++ dp-app/app/controllers/Identity.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import com.hortonworks.dataplane.commons.domain.JsonFormatters._
+import internal.auth.Authenticated
+import play.api.libs.json.Json
+import play.api.mvc._
+import scala.concurrent.Future
+
+class Identity ()
+  extends Controller {
+
+  def list = Authenticated.async { request =>
+    Future.successful(Ok(Json.toJson(request.user)))
+  }
+}
