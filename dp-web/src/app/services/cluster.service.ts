aaa7aaf2f592b0aed4560e31879cc4154158961a
Abhishek Kumar <labs.abhi@github.com>
2017-04-04 21:16:20 +0530

    health working

--- dp-web/src/app/services/cluster.service.ts
+++ dp-web/src/app/services/cluster.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Http, Headers, RequestOptions} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
-import {Cluster, ClusterHealth} from '../models/cluster';
+import {Cluster, ClusterHealthSummary} from '../models/cluster';
 import {HttpUtil} from '../shared/utils/httpUtil';
 
 @Injectable()
@@ -30,16 +30,13 @@ export class ClusterService {
     return Observable.of(true);
   }
 
-  retrieveHealth(clusterId: number): Observable<ClusterHealth>  {
-    // TODO: fix errors
-    const health = new ClusterHealth();
-    health.id = 1;
-    health.status = 'UP';
-    health.state = 'UP';
-    health.uptime = 21312321;
-    health.started = '2017-02-01T22:23:24Z';
-    health.clusterId = 6;
-    return Observable.of(health);
+  retrieveHealth(clusterId: number): Observable<ClusterHealthSummary>  {
+    const uri = `${this.uri}/${clusterId}/health?summary=true`;
+
+    return this.http
+      .get(uri, new RequestOptions(HttpUtil.getHeaders()))
+      .map(HttpUtil.extractData)
+      .catch(HttpUtil.handleError);
   }
 
 }
