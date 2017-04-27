ee643aec563ac9e9fa83226b01393a5011e17975
Ashwin <ashwin@vidyash.com>
2017-04-27 18:27:29 +0200

    BUG-76246 Updated Table

--- services/db-service/db/migrations/V1__dataplane_schema_init.sql
+++ services/db-service/db/migrations/V1__dataplane_schema_init.sql
@@ -87,13 +87,15 @@ COMMENT ON TABLE dataplane.dp_cluster_services  IS 'Services required for DP dis
 
 CREATE TABLE IF NOT EXISTS dataplane.dp_cluster_service_endpoint (
   id          BIGSERIAL PRIMARY KEY,
-  name         VARCHAR(255),
+  name        VARCHAR(255),
   protocol    VARCHAR(8) NOT NULL DEFAULT 'TCP',
   host        VARCHAR(255) NOT NULL ,
   port        INT,
+  pathsegment   TEXT,
   serviceid   BIGINT REFERENCES dataplane.dp_cluster_services(id)
 );
 
+COMMENT ON COLUMN dataplane.dp_cluster_service_endpoint.urlsuffix  IS 'An additional path segment if required';
 COMMENT ON COLUMN dataplane.dp_cluster_service_endpoint.name  IS 'The property key to identify this endpoint';
 COMMENT ON TABLE dataplane.dp_cluster_service_endpoint  IS 'Service endpoints for services listed in dp_cluster_services';
 
