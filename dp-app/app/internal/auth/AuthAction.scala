f2d7797db92eb6c2011fd21b67a9ad4168800db1
Belliraj HB <belliraj.hb@tavant.com>
2017-05-05 23:44:49 +0530

    review comments fix and more refactoring and made fix so that userService is not called once the user is authenticated

--- dp-app/app/internal/auth/AuthAction.scala
+++ dp-app/app/internal/auth/AuthAction.scala
@@ -20,39 +20,31 @@ import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 // TODO: try http://stackoverflow.com/a/29505015/640012
 
+class Authenticated @Inject()(@Named("userService") userService: UserService,
+                              configuration: Configuration,
+                              knoxSso: KnoxSso)
+    extends ActionBuilder[AuthenticatedRequest] {
 
-class Authenticated  @Inject()(@Named("userService") userService: UserService,
-                               configuration: Configuration,
-                               knoxSso:KnoxSso) extends ActionBuilder[AuthenticatedRequest] {
+  private val apiCallTimeout =
+    configuration.underlying.getLong("apicall.timeout").millis
+  private val ssoLoginValidCookieName = "sso_login_valid"
 
-  private val apiCallTimeout = configuration.underlying.getLong("apicall.timeout").millis
-  private val ssoLoginValidCookieName="sso_login_valid"
-
-  def invokeBlock[A](request: Request[A], block: (AuthenticatedRequest[A]) => Future[Result]) = {
-    if (knoxSso.isSsoConfigured() && !request.cookies.get(knoxSso.getSsoCookieName).isDefined){
-      Logger.info(s"Sso is configured but ssocookie ${knoxSso.getSsoCookieName} is not found. " +
-        s"Please check the domain name or sub domain of knox and dp app")
+  def invokeBlock[A](request: Request[A],
+                     block: (AuthenticatedRequest[A]) => Future[Result]) = {
+    if (knoxSso.isSsoConfigured() && !request.cookies
+          .get(knoxSso.getSsoCookieName)
+          .isDefined) {
+      Logger.info(
+        s"Sso is configured but ssocookie ${knoxSso.getSsoCookieName} is not found. " +
+          s"Please check the domain name or sub domain of knox and dp app")
     }
-    if (knoxSso.isSsoConfigured && request.cookies.get(knoxSso.getSsoCookieName).isDefined) {
+    if (knoxSso.isSsoConfigured && request.cookies
+          .get(knoxSso.getSsoCookieName)
+          .isDefined) {
       Logger.debug("sso cookie is found")
-      val serializedJWT: String = request.cookies.get(knoxSso.getSsoCookieName).get.value
-      val jwtValidation: Either[Exception, (String, Long)] = knoxSso.validateJwt(serializedJWT)
-      jwtValidation match {
-        case Left(errors) =>
-          Future.successful(Results.Unauthorized.discardingCookies(DiscardingCookie(ssoLoginValidCookieName)))
-        case Right(jwtResp) =>
-          val (subject,tokenExpiry)=jwtResp
-          val userOption:Option[User]=getUser(subject)
-          if (userOption.isDefined){
-            val respFuture = block(AuthenticatedRequest[A](userOption.get, request))
-            val cookie = Cookie(ssoLoginValidCookieName, "true", None, "/", None, false, false) //Todo set the expiration time got from token if required.
-            respFuture.map(_.withCookies(cookie))
-          }else{
-            Future.successful(Results.Unauthorized.discardingCookies(DiscardingCookie(ssoLoginValidCookieName)))
-          }
-      }
-    } else {
-      val userOption:Option[User] =getUserFromBearerToken(request)
+      authenticateViaKnoxSso(request, block)
+    }else{
+      val userOption: Option[User] = getUserFromBearerToken(request)
       if (userOption.isDefined)
         block(AuthenticatedRequest[A](userOption.get, request))
       else
@@ -60,23 +52,69 @@ class Authenticated  @Inject()(@Named("userService") userService: UserService,
     }
   }
 
-  private def getUser(userName:String):Option[User]={
-    val userFuture: Future[Either[Errors, User]] = userService.loadUser(userName)
-    val userOp: Either[Errors, User] = Await.result(userFuture, apiCallTimeout)
-    userOp.fold(
-      errors => {
-        if (errors.errors.size>0 && errors.errors(0).code.equals("404")){
-          Logger.info( s"User with name[${userName}] not found in dataplane db. users authroized from external system" +
-            s" need to be synced to db.")
+  private def authenticateViaKnoxSso[A](request: Request[A], block: (AuthenticatedRequest[A]) => Future[Result]) = {
+    val serializedJWT: String =
+      request.cookies.get(knoxSso.getSsoCookieName).get.value
+    val jwtValidation: Either[Exception, (String, Long)] =
+      knoxSso.validateJwt(serializedJWT)
+    jwtValidation match {
+      case Left(errors) =>
+        Future.successful(
+          Results.Unauthorized.discardingCookies(
+            DiscardingCookie(ssoLoginValidCookieName)))
+      case Right(jwtResp) =>
+        val (subject, tokenExpiry) = jwtResp
+        val userOptionFromBearerToken: Option[User] = getUserFromBearerToken(
+          request)
+        userOptionFromBearerToken match {
+          case Some(user) =>
+            block(AuthenticatedRequest[A](user, request))
+          case None => {
+            Logger.info(
+              " bearer token not present.fetching user details from db for authentication.")
+            getUserFromDb(subject).flatMap { userOption =>
+              if (userOption.isDefined) {
+                val respFuture =
+                  block(AuthenticatedRequest[A](userOption.get, request))
+                val cookie = Cookie(
+                  ssoLoginValidCookieName,
+                  "true",
+                  None,
+                  "/",
+                  None,
+                  false,
+                  false) //Todo set the expiration time got from token if required.
+                respFuture.map(_.withCookies(cookie))
+              } else {
+                Future.successful(
+                  Results.Unauthorized.discardingCookies(
+                    DiscardingCookie(ssoLoginValidCookieName)))
+              }
+            }
+          }
         }
-        Logger.error(s"Error while retrieving user ${errors}")
-        //TODO: domain and https to be done for proper deletiong of cookie.
-        None
-      },
-      user => Some(user))
+    }
   }
 
-  private def getUserFromBearerToken[A](request: Request[A]):Option[User]={
+  private def getUserFromDb(userName: String): Future[Option[User]] = {
+    userService.loadUser(userName).map { userOp =>
+      userOp match {
+        case Left(errors) => {
+          if (errors.errors.size > 0 && errors.errors(0).code.equals("404")) {
+            Logger.info(
+              s"User with name[${userName}] not found in dataplane db. users authroized from external system" +
+                s" need to be synced to db.")
+          }
+          Logger.error(s"Error while retrieving user ${errors}")
+          //TODO: domain and https to be done for proper deletiong of cookie.
+          None
+        }
+        case Right(user) => Some(user)
+      }
+    }
+  }
+
+  private def getUserFromBearerToken[A](request: Request[A]): Option[User] = {
     val token: Option[String] = getBearerToken(request)
     if (token.isDefined)
       Jwt.parseJWT(token.get)
@@ -84,11 +122,13 @@ class Authenticated  @Inject()(@Named("userService") userService: UserService,
       None
   }
 
-  private def getBearerToken[A](request: Request[A]) :Option[String]= {
-    if (!request.headers.get("Authorization").isDefined || request.headers.get("Authorization").get.startsWith("Bearer "))
+  private def getBearerToken[A](request: Request[A]): Option[String] = {
+    if (!request.headers.get("Authorization").isDefined || !request.headers.get("Authorization").get.startsWith("Bearer "))
       None
-    val header: String = request.headers.get("Authorization").get
-    Some(header.replace("Bearer", "").trim)
+    else{
+      val header: String = request.headers.get("Authorization").get
+      Some(header.replace("Bearer", "").trim)
+    }
   }
 }
 
@@ -111,6 +151,7 @@ object AuthenticatedRequest {
     val body = r.body
     val user = u
     override def secure: Boolean = r.secure
-    override def clientCertificateChain: Option[Seq[X509Certificate]] = r.clientCertificateChain
+    override def clientCertificateChain: Option[Seq[X509Certificate]] =
+      r.clientCertificateChain
   }
 }
