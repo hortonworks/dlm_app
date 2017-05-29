776e5a9a5d1449c4e4bf86c110aa72177b45c531
Ashwin <ashwin@vidyash.com>
2017-05-29 16:53:57 +0200

    DP-BUG-81868 Proxy Ambari API's

--- services/gateway/src/main/java/com/hortonworks/dataplane/gateway/domain/Constants.java
+++ services/gateway/src/main/java/com/hortonworks/dataplane/gateway/domain/Constants.java
@@ -1,12 +1,12 @@
 package com.hortonworks.dataplane.gateway.domain;
 
 
-public interface Constants {
-    String DPAPP = "dpapp";
-    String DPAPP_BASE_PATH="/api/app";
-    String GATEWAY_TOKEN = "gateway-token";
-    String SSO_CHECK_COOKIE_NAME="sso_login_valid";
-    String KNOX_LOGIN_PATH =DPAPP_BASE_PATH+ "/auth/signInThrougKnox";
-    String KNOX_LOGOUT_PATH =DPAPP_BASE_PATH+"/auth/signOutThrougKnox";
+public class Constants {
+    public static String DPAPP = "dpapp";
+    public static String DPAPP_BASE_PATH="/api/app";
+    public static String GATEWAY_TOKEN = "gateway-token";
+    public static String SSO_CHECK_COOKIE_NAME="sso_login_valid";
+    public static String KNOX_LOGIN_PATH =DPAPP_BASE_PATH+ "/auth/signInThrougKnox";
+    public static String KNOX_LOGOUT_PATH =DPAPP_BASE_PATH+"/auth/signOutThrougKnox";
 
 }
