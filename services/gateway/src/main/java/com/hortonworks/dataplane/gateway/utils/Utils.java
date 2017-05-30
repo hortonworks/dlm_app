2bb38fe349cac95cdb6a92b723fdb4f8368b3094
Belliraj HB <belliraj.hb@tavant.com>
2017-05-30 17:23:51 +0530

    refactored gateway code to use base64 encode header value rather than JWT for upstream apps to get user context

--- services/gateway/src/main/java/com/hortonworks/dataplane/gateway/utils/Utils.java
+++ services/gateway/src/main/java/com/hortonworks/dataplane/gateway/utils/Utils.java
@@ -1,8 +1,11 @@
 package com.hortonworks.dataplane.gateway.utils;
 
 import com.google.common.base.Optional;
+import com.hortonworks.dataplane.gateway.domain.Constants;
 import com.netflix.zuul.context.RequestContext;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.zuul.filters.ProxyRequestHelper;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
@@ -14,12 +17,13 @@ import java.net.URISyntaxException;
 @Component
 public class Utils {
   private static final int BEARER_TOK_LEN = "Bearer ".length();
-  private static final String AUTHORIZATION_HEADER = "Authorization";
   private static final String BEARER_TOKEN_KEY = "Bearer ";
+  @Autowired
+  private ProxyRequestHelper proxyRequestHelper;
 
   public Optional<String> getBearerToken() {
     HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
-    String AuthHeader = request.getHeader(AUTHORIZATION_HEADER);
+    String AuthHeader = request.getHeader(Constants.AUTHORIZATION_HEADER);
     if (StringUtils.isNotBlank(AuthHeader) && AuthHeader.startsWith(BEARER_TOKEN_KEY)) {
       String token = request.getHeader("Authorization").substring(BEARER_TOK_LEN);
       return Optional.of(token);
@@ -38,6 +42,9 @@ public class Utils {
       }
     }
     return Optional.absent();
+  }
+  public void deleteAuthorizationHeaderToUpstream(){
+    proxyRequestHelper.addIgnoredHeaders(Constants.AUTHORIZATION_HEADER);
   }
   public Object sendUnauthorized() {
     RequestContext ctx = RequestContext.getCurrentContext();
