a175e818b9b6d7e0539b3fea551897aada3740b3
Belliraj HB <belliraj.hb@tavant.com>
2017-05-31 15:20:51 +0530

    All singing and verification part in gateway. Also more secured signing key

---
+++ services/gateway/src/main/java/com/hortonworks/dataplane/gateway/utils/GatewayKeystore.java
@@ -0,0 +1,94 @@
+package com.hortonworks.dataplane.gateway.utils;
+
+import com.google.common.base.Optional;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.*;
+import java.security.*;
+import java.security.cert.CertificateException;
+
+@Component
+public class GatewayKeystore {
+  private static final Logger logger = LoggerFactory.getLogger(GatewayKeystore.class);
+
+  @Value("${jwt.signing.keystore.jks.path}")
+  private String signingKeystorePath;
+
+  @Value("${jwt.signing.alias}")
+  private String signingKeyAlias;
+
+  @Value("${jwt.signing.keystore.password}")
+  private String signingKeyStorePass;
+  @Value("${jwt.signing.alias.password}")
+  private String signingKeyAliasPass;
+
+  public Key getPrivate(){
+    try {
+      String aliasPass= StringUtils.isBlank(signingKeyAliasPass)?signingKeyStorePass:signingKeyAliasPass;
+      return getKeystore().getKey(signingKeyAlias,aliasPass.toCharArray());
+    } catch (KeyStoreException e) {
+      logger.error("exception",e);
+      throw new RuntimeException(e);
+    } catch (NoSuchAlgorithmException e) {
+      logger.error("exception",e);
+      throw new RuntimeException(e);
+    } catch (UnrecoverableKeyException e) {
+      logger.error("exception",e);
+      throw new RuntimeException(e);
+    }
+  }
+  public PublicKey getPublic(){
+    try {
+      return getKeystore().getCertificate(signingKeyAlias).getPublicKey();
+    } catch (KeyStoreException e) {
+      logger.error("exception",e);
+      throw new RuntimeException(e);
+    }
+  }
+
+  private KeyStore getKeystore(){
+    InputStream keystoreStream=null;
+    try {
+      KeyStore  keyStore = KeyStore.getInstance("JKS");
+      keystoreStream = getKeystoreStream();
+      keyStore.load(keystoreStream, signingKeyStorePass.toCharArray() );
+      return keyStore;
+    } catch (KeyStoreException e) {
+      logger.error("exception",e);
+      throw new RuntimeException(e);
+    } catch (CertificateException e) {
+      logger.error("exception",e);
+      throw new RuntimeException(e);
+    } catch (NoSuchAlgorithmException e) {
+      logger.error("exception",e);
+      throw new RuntimeException(e);
+    } catch (FileNotFoundException e) {
+      logger.error("exception",e);
+      throw new RuntimeException(e);
+    } catch (IOException e) {
+      logger.error("exception",e);
+      throw new RuntimeException(e);
+    }finally {
+      try {
+        if (keystoreStream!=null){
+          keystoreStream.close();
+        }
+      } catch (IOException e) {
+        logger.error("Exception",e);
+      }
+    }
+  }
+
+  private InputStream getKeystoreStream() throws FileNotFoundException {
+    if (StringUtils.isBlank(signingKeystorePath)){
+      return  GatewayKeystore.class.getClassLoader().getResourceAsStream("cert/gateway-signing.jks");
+    }else{
+      final File keyStoreFile = new File( signingKeystorePath );
+      return new FileInputStream(keyStoreFile);
+    }
+  }
+}
