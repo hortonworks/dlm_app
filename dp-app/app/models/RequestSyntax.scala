a6855a1dd860f0b9ae7f981b489dae621f760d0a
Abhishek Kumar <labs.abhi@github.com>
2017-06-02 09:30:49 +0530

    Created simpler models for UI requests to create datasets

--- dp-app/app/models/RequestSyntax.scala
+++ dp-app/app/models/RequestSyntax.scala
@@ -6,6 +6,30 @@ import com.hortonworks.dataplane.commons.domain.JsonFormatters._
 
 object RequestSyntax {
   case class RegisterLake(lake: Datalake, cluster: Cluster)
+  /*
+  {
+    "name":"my ds name",
+    "description":"some description",
+    "lakeId":3,
+    "tags":["tag-1","tag-2","tag-new"],
+    "query":{
+      "filters":  [{
+        "attribute":"asset.source",
+        "attributeType": "string",
+        "operator":"=",
+        "operand":"hive"
+      },{
+        "attribute":"asset.name",
+        "attributeType": "string",
+        "operator":"contains",
+        "operand":"searchText"
+     }]
+    }
+  }
+   */
+  case class DataAssetQueryFilter(attribute: String, attributeType: String, operator: String, operand: String)
+  case class DataAssetQuery(filters: Option[Seq[DataAssetQueryFilter]])
+  case class CreateDataset(name: String, description: String, lakeId: Long, tags: Option[Seq[String]], query: DataAssetQuery)
 }
 
 object Formatters {
@@ -13,4 +37,14 @@ object Formatters {
 
   implicit val registerLakeWrites = Json.writes[RegisterLake]
   implicit val registerLakeReads = Json.reads[RegisterLake]
+
+  implicit val dataAssetQueryFilterWrites = Json.writes[DataAssetQueryFilter]
+  implicit val dataAssetQueryFilterReads = Json.reads[DataAssetQueryFilter]
+
+  implicit val dataAssetQueryWrites = Json.writes[DataAssetQuery]
+  implicit val dataAssetQueryReads = Json.reads[DataAssetQuery]
+
+  implicit val createDatasetWrites = Json.writes[CreateDataset]
+  implicit val createDatasetReads = Json.reads[CreateDataset]
+
 }
