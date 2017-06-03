96209cb753475f6cf2622c0ba7bd5d2c6dcab785
Ashwin <ashwin@vidyash.com>
2017-06-03 15:30:23 +0200

    Datalake -> DPCluster (Part 2)

--- services/db-service/db/migrations/V5__dataplane_indexes_init.sql
+++ services/db-service/db/migrations/V5__dataplane_indexes_init.sql
@@ -1,17 +1,17 @@
 -- Add your indexes here
 
 --Users
-CREATE INDEX IF NOT EXISTS idx_dp_user_username on dataplane.dp_users(user_name);
-CREATE INDEX IF NOT EXISTS idx_dp_user_displayname on dataplane.dp_users(display_name);
+CREATE INDEX IF NOT EXISTS idx_dp_user_username on dataplane.users(user_name);
+CREATE INDEX IF NOT EXISTS idx_dp_user_displayname on dataplane.users(display_name);
 
 --Roles
-CREATE INDEX IF NOT EXISTS idx_dp_roles_name on dataplane.dp_roles(name);
+CREATE INDEX IF NOT EXISTS idx_dp_roles_name on dataplane.roles(name);
 
 -- Configs
-CREATE INDEX IF NOT EXISTS idx_dp_configs on dataplane.dp_configs(config_key);
+CREATE INDEX IF NOT EXISTS idx_dp_configs on dataplane.configs(config_key);
 
 -- Locations
-CREATE INDEX IF NOT EXISTS idx_dp_locations on dataplane.dp_locations(city);
+CREATE INDEX IF NOT EXISTS idx_dp_locations on dataplane.locations(city);
 
 -- cluster services
- CREATE INDEX IF NOT EXISTS idx_dp_clusterservices_servicename on dataplane.dp_cluster_services(service_name);
+ CREATE INDEX IF NOT EXISTS idx_dp_clusterservices_servicename on dataplane.cluster_services(service_name);
