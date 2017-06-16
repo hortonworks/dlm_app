09f39a5da3cf6e2be64f2d0d5df579ba305d6a61
Belliraj HB <belliraj.hb@tavant.com>
2017-06-16 12:45:28 +0530

    refactoring by moving user-roles code to UserRepo from RolesRep

--- services/db-service/app/domain/RoleRepo.scala
+++ services/db-service/app/domain/RoleRepo.scala
@@ -7,14 +7,13 @@ import com.hortonworks.dataplane.commons.domain.Entities.{Role, UserRole, UserRo
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 
 import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
 
-class RoleRepo @Inject()(protected val userRepo: UserRepo, protected val dbConfigProvider: DatabaseConfigProvider) extends HasDatabaseConfigProvider[DpPgProfile] {
+
+class RoleRepo @Inject()( protected val dbConfigProvider: DatabaseConfigProvider) extends HasDatabaseConfigProvider[DpPgProfile] {
 
   import profile.api._
 
   val Roles = TableQuery[RolesTable]
-  val UserRoles = TableQuery[UserRolesTable]
 
   def all(): Future[List[Role]] = db.run {
     Roles.to[List].result
@@ -24,24 +23,6 @@ class RoleRepo @Inject()(protected val userRepo: UserRepo, protected val dbConfi
     val role = Role(roleName = roleName)
     db.run {
       Roles returning Roles += role
-    }
-  }
-
-  def getRolesForUser(userName: String): Future[UserRoles] = {
-    val query = for {
-      users <- userRepo.Users if users.username === userName
-      roles <- Roles
-      userRoles <- UserRoles if roles.id === userRoles.roleId if users.id === userRoles.userId
-    } yield (roles.roleName)
-
-    val result = db.run(query.result)
-    result.map(r => com.hortonworks.dataplane.commons.domain.Entities.UserRoles(userName, r))
-  }
-
-
-  def addUserRole(userRole: UserRole): Future[UserRole] = {
-    db.run {
-      UserRoles returning UserRoles += userRole
     }
   }
 
@@ -61,20 +42,6 @@ class RoleRepo @Inject()(protected val userRepo: UserRepo, protected val dbConfi
     def * = (id, roleName, created, updated) <> ((Role.apply _).tupled, Role.unapply)
   }
 
-   final class UserRolesTable(tag: Tag) extends Table[(UserRole)](tag, Some("dataplane"), "users_roles") {
-    def id = column[Option[Long]]("id", O.PrimaryKey, O.AutoInc)
-
-    def userId = column[Option[Long]]("user_id")
-
-    def roleId = column[Option[Long]]("role_id")
-
-    def user = foreignKey("user_userRole", userId, userRepo.Users)(_.id)
-
-    def role = foreignKey("role_userRole", roleId, Roles)(_.id)
-
-    def * = (id, userId, roleId) <> ((UserRole.apply _).tupled, UserRole.unapply)
-
-  }
 
 
 }
