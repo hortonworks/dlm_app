d144d5c73cfb647b72db5138d8c8f405e9420423
Abhishek Kumar <pr.abhi@yahoo.com>
2017-06-20 14:42:29 +0530

    Removed old files (#301)

--- .attic/dp-web/src/app/components.old/data-analyst/view-data-set/view-data-set.route.ts
+++
@@ -1,7 +0,0 @@
-import { Routes, RouterModule } from '@angular/router';
-import { ModuleWithProviders }  from '@angular/core';
-import { ViewDataSetComponent } from './view-data-set.component';
-
-export const routing: ModuleWithProviders = RouterModule.forChild([
-    { path: '', component: ViewDataSetComponent}
-]);
