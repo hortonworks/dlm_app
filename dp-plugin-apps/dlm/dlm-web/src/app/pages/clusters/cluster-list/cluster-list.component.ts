fdde434762edf5cdd6360fe3d2e01ac011c842c3
Vivek Ratnavel Subramanian <vivekratnavel90@gmail.com>
2017-06-21 18:31:18 -0700

    BUG-79910. UI Tweaks - Clusters Page

--- dp-plugin-apps/dlm/dlm-web/src/app/pages/clusters/cluster-list/cluster-list.component.ts
+++ dp-plugin-apps/dlm/dlm-web/src/app/pages/clusters/cluster-list/cluster-list.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input, ViewChild, TemplateRef, ViewEncapsulation, HostBinding } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, TemplateRef, ViewEncapsulation, HostBinding, ChangeDetectorRef } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Cluster } from 'models/cluster.model';
 import { PoliciesCountEntity } from 'models/policies-count-entity.model';
 import { PairsCountEntity } from 'models/pairs-count-entity.model';
 import { TableTheme } from 'common/table/table-theme.type';
 import { TableComponent } from 'common/table/table.component';
+import { ColumnMode } from '@swimlane/ngx-datatable';
 
 @Component({
   selector: 'dlm-cluster-list',
@@ -16,6 +17,7 @@ export class ClusterListComponent implements OnInit {
   tableTheme = TableTheme.Cards;
   columns = [];
   hdfsRootPath = '/';
+  columnMode = ColumnMode.flex;
 
   @Input() clusters: Cluster[];
 
@@ -31,23 +33,29 @@ export class ClusterListComponent implements OnInit {
 
   @HostBinding('class') className = 'dlm-cluster-list';
 
-  constructor(private t: TranslateService) { }
+  constructor(private t: TranslateService, private cdRef: ChangeDetectorRef) { }
 
   ngOnInit() {
     this.columns = [
-      {name: '', ...TableComponent.makeFixedWith(20)}, // just for padding
-      {prop: 'status', name: this.t.instant('common.status.self'), cellTemplate: this.statusCellRef},
-      {prop: 'name', name: '', cellTemplate: this.nameCellRef},
-      {prop: 'stats', name: this.t.instant('page.clusters.card.usage'), cellTemplate: this.usageCellRef, minWidth: 160},
-      {prop: 'totalHosts', name: this.t.instant('page.clusters.card.nodes'), cellTemplate: this.plainCellRef},
-      {prop: 'pairsCounter', name: this.t.instant('common.pairs'), cellTemplate: this.plainCellRef, maxWidth: 80},
-      {prop: 'policiesCounter', name: this.t.instant('common.policies'), cellTemplate: this.plainCellRef, maxWidth: 90},
-      {prop: 'location', name: this.t.instant('page.clusters.card.location'), cellTemplate: this.locationCellRef},
-      {name: '', cellTemplate: this.addActionsCellRef, ...TableComponent.makeFixedWith(230), cellClass: 'add-actions-cell'}
+      {prop: 'status', name: this.t.instant('common.status.self'), cellTemplate: this.statusCellRef,
+        flexGrow: 2, cellClass: 'status'},
+      {prop: 'name', name: '', cellTemplate: this.nameCellRef, flexGrow: 2},
+      {prop: 'stats', name: this.t.instant('page.clusters.card.usage'), cellTemplate: this.usageCellRef, minWidth: 160, flexGrow: 2},
+      {prop: 'totalHosts', name: this.t.instant('page.clusters.card.nodes'),
+        cellTemplate: this.plainCellRef, flexGrow: 1, cellClass: 'text-center'},
+      {prop: 'pairsCounter', name: this.t.instant('common.pairs'),
+        cellTemplate: this.plainCellRef, maxWidth: 80, flexGrow: 1, cellClass: 'text-center'},
+      {prop: 'policiesCounter', name: this.t.instant('common.policies'),
+        cellTemplate: this.plainCellRef, maxWidth: 80, flexGrow: 1, cellClass: 'text-center'},
+      {prop: 'location', name: this.t.instant('page.clusters.card.location'),
+        cellTemplate: this.locationCellRef, flexGrow: 2},
+      {name: '', cellTemplate: this.addActionsCellRef, ...TableComponent.makeFixedWith(230),
+        cellClass: 'add-actions-cell', flexGrow: 2}
     ];
   }
 
   toggleClusterDetails(clusterRow) {
     this.tableComponent.toggleRowDetail(clusterRow);
+    this.cdRef.detectChanges();
   }
 }
