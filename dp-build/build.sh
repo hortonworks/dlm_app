22fd741d806efe26b7f78a9679a8ac347dceebfe
belliraj <belliraj@gmail.com>
2017-06-29 13:12:25 +0000

    added build for knox agent

--- dp-build/build.sh
+++ dp-build/build.sh
@@ -13,6 +13,7 @@ clean_build() {
 	rm -rf build
 	mkdir -p ${DP_DOCKER_ROOT_FOLDER}/dp-db-service
 	mkdir -p ${DP_DOCKER_ROOT_FOLDER}/dp-app/dp-web
+        mkdir -p ${DP_DOCKER_ROOT_FOLDER}/dp-knox-agent
 	mkdir -p ${DP_DOCKER_ROOT_FOLDER}/dp-knox
 	mkdir -p ${DP_DOCKER_ROOT_FOLDER}/dp-cluster-service
  	mkdir -p ${DP_DOCKER_ROOT_FOLDER}/dp-gateway
@@ -59,7 +60,14 @@ build_db_service() {
 	cp docker_service_start.sh ../../dp-build/${DP_DOCKER_ROOT_FOLDER}/dp-db-service/docker_service_start.sh
 	popd
 }
-
+build_knox_agent() {
+	log "Building Knox Agent"
+	rm -rf ../services/knox-agent/build
+	mkdir ../services/knox-agent/build
+	pushd ../services/knox-agent
+	unpack_for_docker_deploy build/tmp_dp-knox-agent ../../dp-build/${DP_DOCKER_ROOT_FOLDER}/dp-knox-agent/dp-knox-agent
+	popd
+}
 build_dp_gateway() {
 	log "Building gateway"
 	pushd ../services/gateway
@@ -175,6 +183,7 @@ build_dp_configurator
 build_db_service
 build_dp_app
 build_dp_web
+build_knox_agent
 build_dp_knox
 build_cluster_service
 build_installer
