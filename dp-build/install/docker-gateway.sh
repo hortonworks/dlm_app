490a3b08de913136ad20eea489b7b3774303b146
Abhishek Kumar <labs.abhi@github.com>
2017-07-13 14:52:05 +0530

    Replacing compose files with their shell counterparts

---
+++ dp-build/install/docker-gateway.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# dp-gateway:
+#     image: hortonworks/dp-gateway
+#     ports:
+#       - "8762:8762"
+#     volumes:
+#       - ./certs:/usr/dp-app/conf/cert
+#     environment:
+#       CONSUL_HOST: ${CONSUL_HOST}
+#     command: --knox.url=https://${KNOX_FQDN}:8443
+
+docker start dp-gateway || \
+    docker run \
+        --name dp-gateway \
+        --network=dp \
+        --detach \
+        --publish 8762:8762 \
+        --env CONSUL_HOST \
+        --mount type=bind,readonly=true,source=$(pwd)/certs,target=/usr/dp-app/conf/cert \
+        hortonworks/dp-gateway:$VERSION \
+        --knox.url=https://${KNOX_FQDN}:8443
