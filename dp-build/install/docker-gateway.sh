72f0cee1782a09cbf2cd581c554d1cd4b3352187
Abhishek Kumar <labs.abhi@github.com>
2017-07-17 14:24:06 +0530

    Publishing gateway ports for access from outside

--- dp-build/install/docker-gateway.sh
+++ dp-build/install/docker-gateway.sh
@@ -15,6 +15,7 @@ docker start dp-gateway >> install.log 2>&1 || \
         --name dp-gateway \
         --network=dp \
         --detach \
+        --publish 8762:8762 \
         --env CONSUL_HOST \
         --mount type=bind,readonly=false,source=$(pwd)/certs,target=/usr/dp-app/conf/cert \
         hortonworks/dp-gateway:$VERSION \
