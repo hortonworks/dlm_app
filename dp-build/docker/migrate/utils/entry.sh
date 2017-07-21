e82129cfb40e66256516865cdd045d69f0aaafd0
Abhishek Kumar <labs.abhi@github.com>
2017-07-21 18:12:51 +0530

    Works!

--- dp-build/docker/migrate/utils/entry.sh
+++ dp-build/docker/migrate/utils/entry.sh
@@ -2,7 +2,7 @@
 set -e
 
 FLYWAY_ARGS="-configFile=$FLYWAY_SQL_DIR/flyway-docker.conf"
-printenv
+
 if [ -n "$FLYWAY_URI" ]; then
     FLYWAY_ARGS="$FLYWAY_ARGS -url=$FLYWAY_URI"
 fi
@@ -14,5 +14,5 @@ fi
 if [ -n "$FLYWAY_PASS" ]; then
     FLYWAY_ARGS="$FLYWAY_ARGS -password=$FLYWAY_PASS"
 fi
-echo $FLYWAY_ARGS
+
 flyway $FLYWAY_ARGS $@
