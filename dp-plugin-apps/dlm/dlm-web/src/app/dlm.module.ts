c1d2cb78289cfb2ab7aad96adb7e878f86b19995
Vivek Ratnavel Subramanian <vivekratnavel90@gmail.com>
2017-07-23 14:24:49 -0700

    BUG-83437. Integrate DP Suite Navigation (#416)
    * BUG-83437. Integrate DP Suite Navigation

--- dp-plugin-apps/dlm/dlm-web/src/app/dlm.module.ts
+++ dp-plugin-apps/dlm/dlm-web/src/app/dlm.module.ts
@@ -50,6 +50,7 @@ import { HdfsService } from 'services/hdfs.service';
 import { HiveService } from 'services/hive.service';
 import { NotificationService } from 'services/notification.service';
 import { OverviewJobsExternalFiltersService } from 'services/overview-jobs-external-filters.service';
+import { UserService } from 'services/user.service';
 
 import { MainComponent } from './pages/main/main.component';
 import { DlmComponent } from './dlm.component';
@@ -72,6 +73,7 @@ import { UserDropdownComponent } from './common/user-dropdown/user-dropdown.comp
 import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
 import { NotFoundRouteComponent } from './routes/not-found-route/not-found-route.component';
 import { HdfsBrowserComponent } from './components/hdfs-browser/hdfs-browser.component';
+import { PersonaPopupComponent } from 'common/persona-popup/persona-popup.component';
 
 import { OverviewModule } from './pages/overview/overview.module';
 import { ResourceChartsComponent } from './pages/overview/resource-charts/resource-charts.component';
@@ -221,7 +223,8 @@ import { SimpleNotificationsModule } from 'angular2-notifications';
     ModalDialogComponent,
     ModalDialogBodyComponent,
     NotificationsComponent,
-    LogModalDialogComponent
+    LogModalDialogComponent,
+    PersonaPopupComponent
   ],
   bootstrap: [DlmComponent],
   providers: [
@@ -241,11 +244,12 @@ import { SimpleNotificationsModule } from 'angular2-notifications';
     OverviewJobsExternalFiltersService,
     httpServiceProvider,
     FrequencyPipe,
+    UserService,
     AppConfig,
     {
       provide: APP_INITIALIZER,
       useFactory: appConfigFactory,
-      deps: [AppConfig],
+      deps: [AppConfig, UserService],
       multi: true
     }
   ]
