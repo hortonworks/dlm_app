e148522b59b42764f38f606631a2d9bb498bc481
Abhishek Kumar <labs.abhi@github.com>
2017-07-26 14:50:55 +0530

    Removed IP

--- dp-build/install/config.env.sh
+++ dp-build/install/config.env.sh
@@ -1,4 +1,4 @@
 USE_EXT_DB="no"
-# DATABASE_URI="jdbc:postgresql://10.200.4.211:5432/dataplane"
+# DATABASE_URI="jdbc:postgresql://host_name:5432/dataplane"
 # DATABASE_USER="dp_admin"
 # DATABASE_PASS="dp_admin"
