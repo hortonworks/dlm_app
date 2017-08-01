a16f1d6befa43b0110d073f6eec3129cb66bf304
Abhishek Kumar <labs.abhi@github.com>
2017-08-01 13:51:10 +0530

    Reverting change

--- dp-build/install/config.env.sh
+++ dp-build/install/config.env.sh
@@ -2,7 +2,3 @@ USE_EXT_DB="no"
 # DATABASE_URI="jdbc:postgresql://host_name:5432/dataplane"
 # DATABASE_USER="dp_admin"
 # DATABASE_PASS="dp_admin"
-
-CONSUL_HOST="<host_name>"
-MASTER_PASSWORD="admin"
-USE_TEST_LDAP="yes"
