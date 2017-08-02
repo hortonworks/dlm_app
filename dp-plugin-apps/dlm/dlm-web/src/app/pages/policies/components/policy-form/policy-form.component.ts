81fb8c6e3e0ef758252c907c740b9ad034ac69ee
Oleg Nechiporenko <onechiporenko@apache.org>
2017-08-02 13:16:59 +0300

    BUG-85344. Cannot create policy(HDFS/HIVE) if start and end date are same for run job

--- dp-plugin-apps/dlm/dlm-web/src/app/pages/policies/components/policy-form/policy-form.component.ts
+++ dp-plugin-apps/dlm/dlm-web/src/app/pages/policies/components/policy-form/policy-form.component.ts
@@ -174,6 +174,14 @@ export class PolicyFormComponent implements OnInit, OnDestroy, OnChanges {
     return date.toDate();
   }
 
+  get defaultEndTime(): Date {
+    const date = moment();
+    date.hours(23);
+    date.minutes(59);
+    date.seconds(59);
+    return date.toDate();
+  }
+
   get sourceClusters() {
     return mapToList(this.getClusterEntities(this.pairings));
   }
@@ -267,7 +275,7 @@ export class PolicyFormComponent implements OnInit, OnDestroy, OnChanges {
         schedule: this.policySubmitTypes.SCHEDULE,
         endTime: this.formBuilder.group({
           date: [''],
-          time: [this.defaultTime]
+          time: [this.defaultEndTime]
         }, { validator: this.validateTime }),
         startTime: this.formBuilder.group({
           date: [''],
