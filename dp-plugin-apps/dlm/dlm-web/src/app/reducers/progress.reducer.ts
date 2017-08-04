1fb0a70a35a756c6084644050d40e0e7467fe5fe
Oleg Nechiporenko <onechiporenko@apache.org>
2017-08-04 14:20:25 +0300

    BUG-85551. Newly created policy does not show up automatically in the table (page refresh is required)

--- dp-plugin-apps/dlm/dlm-web/src/app/reducers/progress.reducer.ts
+++ dp-plugin-apps/dlm/dlm-web/src/app/reducers/progress.reducer.ts
@@ -79,6 +79,9 @@ export function reducer(state = initialState, action: Action): State {
   // check if dispatched action is completed action (e.g SUCCESS, FAILURE) and update all entities marked with
   // dispatched action name as completed. This hack is present because of nature angular's http service which emits
   // response object once per several request sent within very short interval (kinda "one run cycle")
+  if (!action.type) {
+    return state;
+  }
   if (isCompletedAction(action)) {
     const requests = mapToList(state.entities).reduce((allRequests, request) => {
       if (request.actionType === originalActionName(action)) {
