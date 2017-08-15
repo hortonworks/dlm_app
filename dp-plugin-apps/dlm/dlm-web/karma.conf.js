8e22b807aa2d802b625f8f049b97d1527fef53d4
Yusaku Sako <yusaku@hortonworks.com>
2017-08-15 13:52:47 -0700

    Revert "BUG-86161. Fix dlm web unit tests (#565)"
    This reverts commit 6ad3ad42c5de8c88210ebd0e18f7f3f667ca5cd0.

--- dp-plugin-apps/dlm/dlm-web/karma.conf.js
+++ dp-plugin-apps/dlm/dlm-web/karma.conf.js
@@ -17,7 +17,6 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
@@ -49,7 +48,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['PhantomJS'],
+    browsers: ['Chrome'],
     singleRun: false
   });
 };
