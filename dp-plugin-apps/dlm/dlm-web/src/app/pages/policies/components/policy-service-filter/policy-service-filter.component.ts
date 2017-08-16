8d2cfbeb2301dbb2304b8b1d7e46afe4f4f42658
Vivek Ratnavel Subramanian <vivekratnavel90@gmail.com>
2017-08-15 23:50:06 -0700

    BUG-85951. Address DLM UI Usability Issues (Part 2)

--- dp-plugin-apps/dlm/dlm-web/src/app/pages/policies/components/policy-service-filter/policy-service-filter.component.ts
+++ dp-plugin-apps/dlm/dlm-web/src/app/pages/policies/components/policy-service-filter/policy-service-filter.component.ts
@@ -7,26 +7,37 @@
  * of all or any part of the contents of this software is strictly prohibited.
  */
 
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { POLICY_TYPES, POLICY_TYPES_LABELS } from 'constants/policy.constant';
 
 @Component({
   selector: 'policy-service-filter',
   templateUrl: './policy-service-filter.component.html',
-  styleUrls: ['./policy-service-filter.component.scss']
+  styleUrls: ['./policy-service-filter.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PolicyServiceFilterComponent {
 
-  activeService: '';
+  activeService: string[] = [];
 
   POLICY_TYPES = POLICY_TYPES;
   POLICY_TYPES_LABELS = POLICY_TYPES_LABELS;
 
   @Output() onFilter: EventEmitter<any> = new EventEmitter();
 
-  filterPoliciesByService(service) {
-    this.activeService = this.activeService === service ? '' : service;
-    this.onFilter.emit(this.activeService);
-  }
+  constructor (private cdRef: ChangeDetectorRef) {}
 
+  filterPoliciesByService(service) {
+    const index = this.activeService.indexOf(service);
+    if (index > -1) {
+      this.activeService.splice(index, 1);
+    } else {
+      this.activeService.push(service);
+    }
+    // Filter values only if any one of the values are checked
+    // Since filter by both the values and filter by none are the same
+    const filterValue = this.activeService.length === 1 ? this.activeService[0] : '';
+    this.onFilter.emit(filterValue);
+    this.cdRef.detectChanges();
+  }
 }
