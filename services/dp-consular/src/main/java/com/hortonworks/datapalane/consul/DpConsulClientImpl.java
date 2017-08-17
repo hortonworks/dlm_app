ef74c0e7df93b26148eb56fa90c61ca645fda947
Belliraj <belliraj@gmail.com>
2017-08-17 15:34:04 +0530

    seeting 2 minute for deregistring critical services

--- services/dp-consular/src/main/java/com/hortonworks/datapalane/consul/DpConsulClientImpl.java
+++ services/dp-consular/src/main/java/com/hortonworks/datapalane/consul/DpConsulClientImpl.java
@@ -45,7 +45,8 @@ public class DpConsulClientImpl implements DpConsulClient {
     newCheck.setName(service.getServiceId() + "-healthCheck");
     newCheck.setHttp(String.format("http://%s:%s/health",service.getHost(),service.getPort()));
     newCheck.setInterval(service.getHealthCheckIntervalInSecs() + "s");
-    //Set a default timeout to 1s
+    newCheck.setDeregisterCriticalServiceAfter("2m");
+   //Set a default timeout to 1s
     newCheck.setTimeout("10s");
     Response<Void> voidResponse = consulClient.agentCheckRegister(newCheck);
     return ConsulResponse.from(voidResponse);
