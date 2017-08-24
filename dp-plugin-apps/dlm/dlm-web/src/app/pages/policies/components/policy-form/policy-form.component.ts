b73f3dd92d638f9d34f5b9631af4352f858aa938
Vivek Ratnavel Subramanian <vivekratnavel90@gmail.com>
2017-08-23 17:11:00 -0700

    BUG-85344. Cannot create policy(HDFS/HIVE) if start and end date are same for run job

--- dp-plugin-apps/dlm/dlm-web/src/app/pages/policies/components/policy-form/policy-form.component.ts
+++ dp-plugin-apps/dlm/dlm-web/src/app/pages/policies/components/policy-form/policy-form.component.ts
@@ -138,6 +138,7 @@ export class PolicyFormComponent implements OnInit, OnDestroy, OnChanges {
   freqRequired = {fieldLabel: 'Frequency'};
   freqLimit = {fieldLabel: 'Frequency'};
   directoryField = {fieldLabel: 'Folder path'};
+  maxBandwidthField = {fieldLabel: 'Maximum Bandwidth'};
   get datePickerOptions(): IMyOptions {
     const yesterday = moment().subtract(1, 'day');
     const today = moment();
@@ -382,7 +383,7 @@ export class PolicyFormComponent implements OnInit, OnDestroy, OnChanges {
     this.policyForm.patchValue({
       job: {
         endTime: {
-          time: moment(this.defaultTime).toDate()
+          time: moment(this.defaultEndTime).toDate()
         },
         startTime: {
           time: moment(this.defaultTime).toDate()
@@ -435,9 +436,14 @@ export class PolicyFormComponent implements OnInit, OnDestroy, OnChanges {
             value.job.startTime.date = moment(startDate).add(1, 'weeks').isoWeekday(dayToLook).format('YYYY-MM-DD');
           }
         }
+        if (value.job.endTime && 'date' in value.job.endTime) {
+          const endDate = value.job.endTime.date;
+          value.job.endTime.time = moment(endDate).hours(23).minutes(59).seconds(59).toDate();
+        }
       }
       const userTimezone = this.timezoneService.userTimezone;
       value.userTimezone =  userTimezone ? userTimezone.label : '';
+      console.log('submit ', value);
       this.formSubmit.emit(value);
     }
     markAllTouched(this.policyForm);
