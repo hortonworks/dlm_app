2a6d57d19a41ccbe397b31643f09a8ef9ae44a50
Abhishek Kumar <labs.abhi@gmail.com>
2017-08-25 16:36:00 +0530

    Improving map

--- dp-web/src/app/modules/infra/widgets/map/map.component.ts
+++ dp-web/src/app/modules/infra/widgets/map/map.component.ts
@@ -30,9 +30,48 @@ class MapEdge {
   ) {}
 }
 
-class MapVertexMarker extends L.Marker {
+const ClusterIcon = L.Icon.extend({
+  options: {
+    // @section
+    // @aka DivIcon options
+    iconSize: [12, 12], // also can be set through CSS
 
-}
+    // iconAnchor: (Point),
+    // popupAnchor: (Point),
+
+    // @option html: String = ''
+    // Custom HTML code to put inside the div element, empty by default.
+    html: false,
+
+    // @option bgPos: Point = [0, 0]
+    // Optional relative position of the background, in pixels
+    bgPos: null,
+
+    className: 'dp-leaflet-clusters-icon',
+
+    clusters: []
+  },
+
+  createIcon: function (oldIcon) {
+    var div = (oldIcon && oldIcon.tagName === 'DIV') ? oldIcon : document.createElement('div'),
+        options = this.options;
+
+    div.innerHTML = options.html !== false ? options.html : '';
+
+    if (options.bgPos) {
+      var bgPos = L.point(options.bgPos);
+      div.style.backgroundPosition = (-bgPos.x) + 'px ' + (-bgPos.y) + 'px';
+    }
+    this._setIconStyles(div, 'icon');
+
+    return div;
+  },
+
+  createShadow: function () {
+    return null;
+  }
+})
+
 
 @Component({
   selector: 'dp-map',
@@ -44,7 +83,7 @@ export class MapComponent implements OnChanges, OnInit {
   map: L.Map;
   @ViewChild('mapcontainer') mapcontainer: ElementRef;
   @Input('mapData') mapData: MapData[] = [];
-  @Input('mapSize') mapSize: MapSize = MapSize.EXTRALARGE;
+  @Input('mapSize') size: MapSize = MapSize.EXTRALARGE;
 
   vertices: MapVertex[];
   edges: MapEdge[];
@@ -59,27 +98,25 @@ export class MapComponent implements OnChanges, OnInit {
   markerColorOuterBorder = '#C4DCEC';
   markerColorInnerBorder = '#FFFFFF';
 
-  mapColor = '#F2F7FC';
+  MAP_COLOR = '#F2F7FC';
 
-  mapOptions = {
+  MAP_OPTIONS = {
     scrollWheelZoom: false,
     zoomControl: false,
     dragging: false,
     boxZoom: false,
     doubleClickZoom: false,
     zoomSnap: 0.1,
-    zoomAnimation: false
+    zoomAnimation: false,
+    attributionControl: false
   };
 
-  markerAndCurveLayer: Layer[] = [];
-  layerGroup: LayerGroup;
-
-  defaultMapSizes: MapDimensions[] = [
-    new MapDimensions('240px', '420px', 0.5),
-    new MapDimensions('420px', '540px', 1),
-    new MapDimensions('480px', '680px', 1.3),
-    new MapDimensions('500px', '59%', 1.54)
-  ];
+  MAP_SIZES = {
+    [MapSize.SMALL] :new MapDimensions('240px', '420px', 0.5),
+    [MapSize.MEDIUM] :new MapDimensions('420px', '540px', 1),
+    [MapSize.LARGE] :new MapDimensions('480px', '680px', 1.3),
+    [MapSize.EXTRALARGE] :new MapDimensions('500px', '59%', 1.54)
+  };
 
   constructor(private geographyService: GeographyService) {
     this.vertices = [
@@ -95,28 +132,15 @@ export class MapComponent implements OnChanges, OnInit {
     if (this.map) {
       this.map.remove();
     }
-    this.geographyService.getCountries().subscribe((countries) => {
-      this.drawMap(countries);
-    });
-  }
+    this.geographyService
+      .getCountries()
+      .subscribe(geoJSON => {
+        const dimension = this.MAP_SIZES[this.size] || this.MAP_SIZES[MapSize.EXTRALARGE];
 
-  drawMap(countries) {
-    let mapDimensions = this.defaultMapSizes[this.mapSize] || this.defaultMapSizes[MapSize.EXTRALARGE];
-    this.mapcontainer.nativeElement.style.height = mapDimensions.height;
-    this.mapcontainer.nativeElement.style.width = mapDimensions.width;
-    const map = L.map(this.mapcontainer.nativeElement, this.mapOptions);
-    let countriesLayer = L.geoJSON(countries, {
-      style: feature => ({
-        fillColor: this.mapColor,
-        fillOpacity: 1,
-        weight: 1,
-        color: this.mapColor
-      })
-    }).addTo(map);
-    map.fitBounds(countriesLayer.getBounds());
-    this.map = map;
-    this.map.setZoom(mapDimensions.zoom);
-    this.mapcontainer.nativeElement.querySelector('.leaflet-map-pane').style.height = `${parseInt(mapDimensions.height, 10) - 20}px`;
+        const container = this.mapcontainer.nativeElement;
+
+        this.map = this.getDrawnMap(container, dimension, geoJSON);
+      });
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -126,94 +150,63 @@ export class MapComponent implements OnChanges, OnInit {
     const vertices = this.vertices;
     const edges = this.edges;
 
-    vertices.forEach(cVertex => this.drawVertex(cVertex));
-    edges.forEach(cEdge => this.drawEdge(vertices, cEdge));
+    const markers = vertices.reduce((markerMap, cVertex) => Object.assign({}, markerMap, {
+      [cVertex.id]: this.getDrawnVertex(cVertex)
+    }), {});
+    const arcs = edges.reduce((arcMap, cEdge) => Object.assign({}, arcMap, {
+      [cEdge.id]: this.getDrawnEdge(vertices, cEdge)
+    }), {});
 
-    this.removeExistingMarker();
-    this.layerGroup = new L.LayerGroup(this.markerAndCurveLayer);
+    const mLayer = L.layerGroup(Object.keys(markers).map(cMarkerKey => markers[cMarkerKey]));
+    this.map.addLayer(mLayer)
   }
 
-  private drawVertex(vertex: MapVertex): L.CircleMarker[] {
+  getDrawnMap(container, dimension, geoJSON): L.Map {
+    const map = L.map(container, this.MAP_OPTIONS);
+    const baseLayer = this.getBaseLayer(geoJSON);
+    map.addLayer(baseLayer);
+    map.fitBounds(baseLayer.getBounds());
+    map.setZoom(dimension.zoom);
+
+    return map;
+  }
+
+  getBaseLayer(geoJSON): L.GeoJSON {
+    return L.geoJSON(geoJSON, {
+      style: feature => ({
+        fillColor: this.MAP_COLOR,
+        fillOpacity: 1,
+        weight: 1,
+        color: this.MAP_COLOR
+      }),
+      pane: 'tilePane',
+    })
+  }
+
+  private getDrawnVertex(vertex: MapVertex): L.CircleMarker {
     const position = L.latLng(vertex.latitude, vertex.longitude);
 
-    L.circleMarker(position, {
+    return L.circleMarker(position, {
       radius: 5,
       color: this.markerColorInnerBorder,
       weight: 2,
-      fillColor: color,
+      fillColor: 'red',
       fillOpacity: 0.8
     })
   }
 
-  private drawEdge(vertices: MapVertex[], edge: MapEdge) {
+  private getDrawnEdge(vertices: MapVertex[], edge: MapEdge) {
     const sourceVertex = vertices.find(cVertex => cVertex.id === edge.sourceId);
     const targetVertex = vertices.find(cVertex => cVertex.id === edge.targetId);
-    this.drawConnection(sourceVertex, targetVertex, edge.meta);
+    return this.drawConnection(sourceVertex, targetVertex, edge.meta);
   }
 
-  plotPoint(position) {
-    let latLng = L.latLng(position.latitude, position.longitude);
-    if (this.markerExists(latLng)) {
-      return;
-    }
-    this.markerLookup.push(latLng);
-    if (position.status === MapConnectionStatus.UP) {
-      this.createMarker(latLng, this.statusColorUp);
-    } else if (position.status === MapConnectionStatus.DOWN) {
-      this.createMarker(latLng, this.statusColorDown);
-    } else {
-      this.createMarker(latLng, this.statusColorNA);
-    }
-  }
-
-  removeExistingMarker() {
-    if (this.layerGroup) {
-      this.layerGroup.eachLayer(layer => {
-        this.map.removeLayer(layer);
-      });
-    }
-    this.markerLookup = [];
-    this.pathLookup = [];
-  }
-
-  createMarker(latLng, color) {
-    let outerMarker = L.circleMarker(latLng, {
-      radius: 14,
-      color: this.markerColorOuterBorder,
-      weight: 2,
-      fillColor: color,
-      fillOpacity: 0.25
-    }).addTo(this.map);
-    let innerMarker = L.circleMarker(latLng, {
-      radius: 5,
-      color: this.markerColorInnerBorder,
-      weight: 2,
-      fillColor: color,
-      fillOpacity: 0.8
-    }).addTo(this.map);
-    this.markerAndCurveLayer.push(outerMarker);
-    this.markerAndCurveLayer.push(innerMarker);
-  }
-
-  markerExists(latLng) {
-    return this.markerLookup.find(marker => {
-      return marker.lat === latLng.lat && marker.lng === latLng.lng;
-    });
-  }
-
-  pathExists(curve) {
-    return this.pathLookup.find(path => {
-      return path.start.latitude === curve.start.latitude && path.start.longitude === curve.start.longitude
-        && path.end.latitude === curve.end.latitude && path.end.longitude === curve.end.longitude;
-    });
-  }
-
-  drawConnection(sourceVertex: MapVertex, targetVertex: MapVertex, meta: object) {
+  drawConnection(sourceVertex: MapVertex, targetVertex: MapVertex, meta: object): L.Path {
     const start = sourceVertex;
     const end = targetVertex;
     let midPoint = {x: (start.latitude + end.latitude) / 2, y: (start.longitude + end.longitude) / 2};
     let distance = Math.sqrt(Math.pow(end.latitude - start.latitude, 2) + Math.pow(end.longitude - start.longitude, 2));
-    let path = L.curve(
+    return L.curve(
       ['M', [start.latitude, start.longitude], 'Q', [midPoint.x + distance / 3, midPoint.y + distance / 3], [end.latitude, end.longitude]],
       {
         color: this.statusColorUp,
@@ -221,11 +214,9 @@ export class MapComponent implements OnChanges, OnInit {
         dashArray: '1, 5',
         weight: 2
       });
-    if (this.pathExists({start: start, end: end}) || this.pathExists({start: end, end: start})) {
-      return;
-    }
-    this.pathLookup.push({start: start, end: end});
-    path.addTo(this.map);
-    this.markerAndCurveLayer.push(path);
+  }
+
+  get mapSizeAsString(): string {
+    return MapSize[this.size];
   }
 }
