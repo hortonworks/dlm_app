ea3f7683a96b6c7e2cd5bbb9155daf56f8000a61
Hemanth Yamijala <yhemanth@gmail.com>
2017-09-07 15:18:38 +0530

    Setting default value for the asset.audit.mock.show to false

--- services/db-service/db/migrations/V4__dataplane_default_configs.sql
+++ services/db-service/db/migrations/V4__dataplane_default_configs.sql
@@ -4,4 +4,4 @@ INSERT INTO dataplane.configs (config_key, config_value, active, export) VALUES 
 INSERT INTO dataplane.configs (config_key, config_value, active, export) VALUES ('dp.atlas.password', 'admin', true, true);
 INSERT INTO dataplane.configs (config_key, config_value, active, export) VALUES ('dp.ranger.user', 'admin', true, true);
 INSERT INTO dataplane.configs (config_key, config_value, active, export) VALUES ('dp.ranger.password', 'admin', true, true);
-INSERT INTO dataplane.configs (config_key, config_value, active, export) VALUES ('asset.audit.mock.show', true, true, true);
+INSERT INTO dataplane.configs (config_key, config_value, active, export) VALUES ('asset.audit.mock.show', 'false', true, true);
