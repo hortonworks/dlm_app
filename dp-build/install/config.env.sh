fd95e0034de362e086ace788bbe6b030adaed664
Hemanth Yamijala <hyamijala@hortonworks.com>
2017-10-18 14:10:36 +0530

    BUG-90377: Turn SSL on by default and remove hardcoded certs password

--- dp-build/install/config.env.sh
+++ dp-build/install/config.env.sh
@@ -8,23 +8,23 @@
 #  * of all or any part of the contents of this software is strictly prohibited.
 #  */
 #
+
+# DB Configs
+USE_EXT_DB="no"
+# DATABASE_URI="jdbc:postgresql://host_name:5432/dataplane"
+# DATABASE_USER="<username>"
+# DATABASE_PASS="<password>"
+
+# SSL Configs
+USE_TLS="true"
+USE_PROVIDED_CERTIFICATES="no"
+# PUBLIC_KEY_L="/absolute/path/of/public/key.pem"
+# PRIVATE_KEY_L="/absolute/path/of/private/key.pem"
+
+# Knox Configs
 SEPARATE_KNOX_CONFIG=false
 KNOX_CONFIG_USING_CREDS=true
 
-USE_EXT_DB="no"
-# DATABASE_URI="jdbc:postgresql://host_name:5432/dataplane"
-# DATABASE_USER="dp_admin"
-# DATABASE_PASS="dp_admin"
-
 # Uncomment and set right values for below lines to enable silent install
 # CONSUL_HOST="<host_ip_address>"
-# MASTER_PASSWORD="changeit"
-# USE_TEST_LDAP="no"
-
-# USE_TLS="true"
-# USE_PROVIDED_CERTIFICATES="yes"
-# PUBLIC_KEY_L="/absolute/path/of/public/key.pem"
-# PRIVATE_KEY_L="/absolute/path/of/private/key.pem"
-# CERTIFICATE_PASSWORD="changeit"
-
-# USER_ADMIN_PASSWORD="admin"
+# USE_TEST_LDAP="yes"
