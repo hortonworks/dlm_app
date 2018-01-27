fca62bd130e0b920a349d5441954df2bfeb500b4
iraghumitra <raghumitra.ksv@gmail.com>
2018-01-28 01:26:52 +0530

    Logout button missing in DSS

--- dp-plugin-apps/dss/dss-web/src/app/shared/header/header.module.ts
+++ dp-plugin-apps/dss/dss-web/src/app/shared/header/header.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HeaderComponent } from './header.component';
 import {BreadCrumbModule} from '../bread-crumb/bread-crumb.module';
+import {RouterModule} from '@angular/router';
 
 
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule,
     BreadCrumbModule
   ],
   declarations: [HeaderComponent],
