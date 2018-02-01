512d39f429a11150e1da212bd74735cf0e28b104
onechiporenko <oo.nechiporenko@gmail.com>
2018-02-01 11:41:09 +0200

    BUG-92834. Create Replication Policy page should account for DLM cross-version (#1028)

--- dp-plugin-apps/dlm/dlm-web/src/app/pages/policies/subpages/create-policy/create-policy.component.ts
+++ dp-plugin-apps/dlm/dlm-web/src/app/pages/policies/subpages/create-policy/create-policy.component.ts
@@ -27,10 +27,18 @@ import { CloudAccount } from 'models/cloud-account.model';
 import { loadContainers } from 'actions/cloud-container.action';
 import { getAllContainers, getAllContainersGrouped } from 'selectors/cloud-container.selector';
 import { CloudContainer } from 'models/cloud-container.model';
+import { loadBeaconAdminStatus } from 'actions/beacon.action';
+import { getAllBeaconAdminStatuses } from 'selectors/beacon.selector';
+import { BeaconAdminStatus } from 'models/beacon-admin-status.model';
+import { loadClusters } from 'actions/cluster.action';
+import { getAllClusters } from 'selectors/cluster.selector';
+import { Cluster } from 'models/cluster.model';
 
 const PAIR_REQUEST = '[CREATE POLICY] PAIR_REQUEST';
+const CLUSTERS_REQUEST = '[CREATE POLICY] CLUSTERS_REQUEST';
 const ACCOUNTS_REQUEST = '[CREATE POLICY] ACCOUNTS_REQUEST';
 const CONTAINERS_REQUEST = '[CREATE POLICY] CONTAINERS_REQUEST';
+const ADMIN_STATUS_REQUEST = '[CREATE POLICY] ADMIN_STATUS_REQUEST';
 
 @Component({
   selector: 'dp-create-policy',
@@ -45,8 +53,10 @@ const CONTAINERS_REQUEST = '[CREATE POLICY] CONTAINERS_REQUEST';
         <div *ngIf="(pairings$ | async)?.length > 0; else noPairs">
           <dlm-policy-form
             [accounts]="accounts$ | async"
+            [clusters]="clusters$ | async"
             [containers]="containersGrouped$ | async"
             [containersList]="containers$ | async"
+            [beaconStatuses]="beaconStatuses$ | async"
             [pairings]="pairings$ | async"
             [sourceClusterId]="sourceClusterId"
             (formSubmit)="handleFormSubmit($event)"
@@ -71,7 +81,9 @@ const CONTAINERS_REQUEST = '[CREATE POLICY] CONTAINERS_REQUEST';
 export class CreatePolicyComponent implements OnInit, OnDestroy {
   pairings$: Observable<Pairing[]>;
   accounts$: Observable<CloudAccount[]>;
+  clusters$: Observable<Cluster[]>;
   containers$: Observable<CloudContainer[]>;
+  beaconStatuses$: Observable<BeaconAdminStatus[]>;
   containersGrouped$: Observable<any>;
   overallProgress$: Observable<ProgressState>;
   loadParamsSubscription$;
@@ -84,14 +96,19 @@ export class CreatePolicyComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.store.dispatch(loadPairings(PAIR_REQUEST));
     this.store.dispatch(loadAccounts(ACCOUNTS_REQUEST));
+    this.store.dispatch(loadClusters(CLUSTERS_REQUEST));
+    this.store.dispatch(loadBeaconAdminStatus({requestId: ADMIN_STATUS_REQUEST}));
     this.pairings$ = this.store.select(getAllPairings);
     this.accounts$ = this.store.select(getAllAccounts);
+    this.clusters$ = this.store.select(getAllClusters);
+    this.beaconStatuses$ = this.store.select(getAllBeaconAdminStatuses);
     this.containersGrouped$ = this.store.select(getAllContainersGrouped);
     this.containers$ = this.store.select(getAllContainers);
     this.loadAccountsSubscription$ = this.accounts$.subscribe(accounts => {
       this.store.dispatch(loadContainers(accounts, CONTAINERS_REQUEST));
     });
-    this.overallProgress$ = this.store.select(getMergedProgress(ACCOUNTS_REQUEST, CONTAINERS_REQUEST, PAIR_REQUEST));
+    const progress = getMergedProgress(ACCOUNTS_REQUEST, CONTAINERS_REQUEST, PAIR_REQUEST, ADMIN_STATUS_REQUEST, CLUSTERS_REQUEST);
+    this.overallProgress$ = this.store.select(progress);
     this.loadParamsSubscription$ = this.route.queryParams
       .subscribe(params => {
         const clusterId = params['sourceClusterId'];
