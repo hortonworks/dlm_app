0d911e1f84fd627f2dbebffddcd0e06e531a3f56
padmapriyanitt <padmapriyanitt@gmail.com>
2018-02-07 14:22:09 +0530

    Moved insert statement to a new schema file

--- services/db-service/db/migrations/V4__dataplane_default_configs.sql
+++ services/db-service/db/migrations/V4__dataplane_default_configs.sql
@@ -1,3 +1,2 @@
 INSERT INTO dataplane.configs (config_key, config_value, active, export) VALUES ('asset.audit.mock.show', 'false', true, true);
 INSERT INTO dataplane.configs (config_key, config_value, active, export) VALUES ('dp.session.timeout.minutes', 120, true, true);
-INSERT INTO dataplane.configs (config_key, config_value, active, export) VALUES ('dps.ga.tracking.enabled', 'true', true, true);
