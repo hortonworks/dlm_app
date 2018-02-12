3bed1baab80c241e89a137428dacfefa0d828b75
Abhishek Kumar <labs.abhi@gmail.com>
2018-02-12 15:57:25 +0530

    Updated keys and code

--- services/gateway/src/main/java/com/hortonworks/dataplane/gateway/utils/GatewayKeystore.java
+++ services/gateway/src/main/java/com/hortonworks/dataplane/gateway/utils/GatewayKeystore.java
@@ -10,94 +10,97 @@
  */
 package com.hortonworks.dataplane.gateway.utils;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.Cipher;
+import javax.crypto.EncryptedPrivateKeyInfo;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import java.io.*;
 import java.security.*;
 import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 
 @Component
 public class GatewayKeystore {
   private static final Logger logger = LoggerFactory.getLogger(GatewayKeystore.class);
 
-  @Value("${jwt.signing.keystore.jks.path}")
-  private String signingKeystorePath;
+  @Value("${jwt.public.key.path}")
+  private String publicKeyPath;
+  @Value("${jwt.private.key.path}")
+  private String privateKeyPath;
+  @Value("${jwt.private.key.password}")
+  private String privateKeyPassword;
 
-  @Value("${jwt.signing.alias}")
-  private String signingKeyAlias;
-
-  @Value("${jwt.signing.keystore.password}")
-  private String signingKeyStorePass;
-  @Value("${jwt.signing.alias.password}")
-  private String signingKeyAliasPass;
-
-  public Key getPrivate(){
+  public PrivateKey getPrivate(){
     try {
-      String aliasPass= StringUtils.isBlank(signingKeyAliasPass)?signingKeyStorePass:signingKeyAliasPass;
-      return getKeystore().getKey(signingKeyAlias,aliasPass.toCharArray());
-    } catch (KeyStoreException e) {
-      logger.error("exception",e);
-      throw new RuntimeException(e);
+      EncryptedPrivateKeyInfo encryptPKInfo = new EncryptedPrivateKeyInfo(getKeyFileAsString(this.privateKeyPath).getBytes());
+
+      Cipher cipher = Cipher.getInstance(encryptPKInfo.getAlgName());
+      PBEKeySpec pbeKeySpec = new PBEKeySpec(this.privateKeyPassword.toCharArray());
+      SecretKeyFactory secFac = SecretKeyFactory.getInstance(encryptPKInfo.getAlgName());
+      Key pbeKey = secFac.generateSecret(pbeKeySpec);
+      AlgorithmParameters algParams = encryptPKInfo.getAlgParameters();
+      cipher.init(Cipher.DECRYPT_MODE, pbeKey, algParams);
+
+      KeySpec pkcs8KeySpec = encryptPKInfo.getKeySpec(cipher);
+      KeyFactory kf = KeyFactory.getInstance("RSA");
+      return kf.generatePrivate(pkcs8KeySpec);
     } catch (NoSuchAlgorithmException e) {
-      logger.error("exception",e);
+      logger.error("Exception", e);
       throw new RuntimeException(e);
-    } catch (UnrecoverableKeyException e) {
-      logger.error("exception",e);
+    } catch (InvalidKeySpecException e) {
+      logger.error("Exception", e);
       throw new RuntimeException(e);
-    }
-  }
-  public PublicKey getPublic(){
-    try {
-      return getKeystore().getCertificate(signingKeyAlias).getPublicKey();
-    } catch (KeyStoreException e) {
-      logger.error("exception",e);
+    } catch (InvalidAlgorithmParameterException e) {
+      logger.error("Exception", e);
       throw new RuntimeException(e);
-    }
-  }
-
-  private KeyStore getKeystore(){
-    InputStream keystoreStream=null;
-    try {
-      KeyStore  keyStore = KeyStore.getInstance("JKS");
-      keystoreStream = getKeystoreStream();
-      keyStore.load(keystoreStream, signingKeyStorePass.toCharArray() );
-      return keyStore;
-    } catch (KeyStoreException e) {
-      logger.error("exception",e);
+    } catch (NoSuchPaddingException e) {
+      logger.error("Exception", e);
       throw new RuntimeException(e);
-    } catch (CertificateException e) {
-      logger.error("exception",e);
-      throw new RuntimeException(e);
-    } catch (NoSuchAlgorithmException e) {
-      logger.error("exception",e);
-      throw new RuntimeException(e);
-    } catch (FileNotFoundException e) {
-      logger.error("exception",e);
+    } catch (InvalidKeyException e) {
+      logger.error("Exception", e);
       throw new RuntimeException(e);
     } catch (IOException e) {
-      logger.error("exception",e);
+      logger.error("Exception", e);
       throw new RuntimeException(e);
-    }finally {
-      try {
-        if (keystoreStream!=null){
-          keystoreStream.close();
-        }
-      } catch (IOException e) {
-        logger.error("Exception",e);
-      }
     }
   }
 
-  private InputStream getKeystoreStream() throws FileNotFoundException {
-    if (StringUtils.isBlank(signingKeystorePath)){
-      return  GatewayKeystore.class.getClassLoader().getResourceAsStream("cert/gateway-signing.jks");
-    }else{
-      final File keyStoreFile = new File( signingKeystorePath );
-      return new FileInputStream(keyStoreFile);
+  public PublicKey getPublic(){
+    try {
+      CertificateFactory fact = CertificateFactory.getInstance("X.509");
+      ByteArrayInputStream is = new ByteArrayInputStream(getKeyFileAsString(this.publicKeyPath).getBytes("UTF8"));
+      X509Certificate cer = (X509Certificate) fact.generateCertificate(is);
+      return cer.getPublicKey();
+    } catch (IOException e) {
+      logger.error("Exception", e);
+      throw new RuntimeException(e);
+    } catch (CertificateException e) {
+      logger.error("Exception", e);
+      throw new RuntimeException(e);
+    }
+  }
+
+  private String getKeyFileAsString(String path) {
+    try {
+      if (!StringUtils.isBlank(path)) {
+        return FileUtils.readFileToString(new File(path));
+      } else {
+        throw new RuntimeException("F");
+      }
+    } catch (IOException e) {
+      logger.error("Exception", e);
+      throw new RuntimeException(e);
     }
   }
 }
