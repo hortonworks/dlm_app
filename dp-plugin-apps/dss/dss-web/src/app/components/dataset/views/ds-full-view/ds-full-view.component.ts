39275eba1b6a27ca5dd0b58f2fc22990bcd8c0bb
ashwini-anand <aayanand@gmail.com>
2018-02-23 16:02:14 +0530

    correcting dpclusterId

--- dp-plugin-apps/dss/dss-web/src/app/components/dataset/views/ds-full-view/ds-full-view.component.ts
+++ dp-plugin-apps/dss/dss-web/src/app/components/dataset/views/ds-full-view/ds-full-view.component.ts
@@ -203,7 +203,7 @@ export class DsFullView implements OnInit {
       dataset.id = this.dsModel.id;
       dataset.createdBy = this.dsModel.creatorId;
       dataset.createdOn = this.dsModel.createdOn;
-      dataset.dpClusterId = this.dsModel.clusterId;
+      dataset.dpClusterId = this.dsModel.datalakeId;
       dataset.datalakeId = this.dsModel.datalakeId;
       dataset.description = this.dsModel.description;
       dataset.lastModified = this.dsModel.lastModified;
