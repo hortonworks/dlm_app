f99077d218624fcf1a8bfb6acc3a745dd8fbfd26
Abhishek Kumar <labs.abhi@gmail.com>
2018-02-26 10:36:00 +0530

    Reverting some things

--- charts/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-kubectl create namespace zabhi-dev
-
-helm install dps-core \
-    --set registry.url=docker.io \
-    --set registry.username=zabhi \
-    --set registry.password=HWdocker@6491 \
-    --namespace=zabhi-dev
