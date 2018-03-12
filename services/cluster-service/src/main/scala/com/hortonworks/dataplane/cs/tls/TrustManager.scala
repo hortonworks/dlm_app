fdbb647f38b5163d0a5ffba717bf55ae574f945e
Abhishek Kumar <labs.abhi@gmail.com>
2018-03-12 14:23:42 +0530

    trust manager

---
+++ services/cluster-service/src/main/scala/com/hortonworks/dataplane/cs/tls/TrustManager.scala
@@ -0,0 +1,45 @@
+package com.hortonworks.dataplane.cs.tls
+
+import javax.inject.Inject
+
+import com.hortonworks.dataplane.db.Webservice.CertificateService
+import com.typesafe.config.Config
+import com.typesafe.sslconfig.ssl.{SSLConfigSettings, TrustManagerConfig, TrustStoreConfig}
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
+import scala.util.Try
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class TrustManager @Inject()(val config: Config, val certificateService: CertificateService) {
+  val system = TrustStoreConfig(None, None)
+    .withFilePath(Some("${java.home}/lib/security/cacerts"))
+
+  val timeout = Duration(Try(config.getString("dp.certificate.query.timeout")).getOrElse("30 seconds)"))
+
+  var tlsConfig = Await.result(build, timeout)
+
+  private def build: Future[SSLConfigSettings] = {
+    certificateService.list(active = Some(true))
+      .map { certificates =>
+        val trusts =
+          certificates
+            .map(cCertificate => TrustStoreConfig(None, None).withStoreType(cCertificate.format).withData(Some(cCertificate.data)))
+
+        val trustManagerConfig =
+          TrustManagerConfig()
+            .withTrustStoreConfigs((trusts :+ system).toList)
+
+        SSLConfigSettings()
+          .withTrustManagerConfig(trustManagerConfig)
+      }
+  }
+
+  def get: SSLConfigSettings = tlsConfig
+
+  def reload: SSLConfigSettings = {
+    tlsConfig = Await.result(build, timeout)
+    tlsConfig
+  }
+
+}
