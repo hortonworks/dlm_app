51cca8f638cbfc933a2d0197671e488e812cef5b
Abhishek Kumar <labs.abhi@gmail.com>
2018-03-12 11:24:05 +0530

    Updated query

--- services/db-service/db/migrations/V10__dataplane_persist_trust.sql
+++ services/db-service/db/migrations/V10__dataplane_persist_trust.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS dataplane.certificates (
-  id      BIGSERIAL PRIMARY KEY,
+  id      BIGSERIAL     PRIMARY KEY,
+  name    VARCHAR(255)  NOT NULL,
   data    BYTEA,
   active  BOOLEAN
 );
